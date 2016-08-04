5e7afee75bbd3f926dddd8fc095551c6dc2e8cfc
opensource1030 <opensource1030@gmail.com>
2016-08-05 02:25:16 +0545

    JRA-1442 #comment dashboard UI/UX inprogress

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+'use strict';
+var $ = require('jquery');
+var foundation = require('../vendor/foundation-sites/dist/foundation.js');
+var foundation = require('../vendor/foundation-sites/dist/plugins/foundation.equalizer.js');
+
+$(document).foundation();
+
+
+
+
--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,23 @@ var sass        = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 var reload      = browserSync.reload;
 const imagemin = require('gulp-imagemin');
+var watchify     = require('watchify');
+var browserify   = require('browserify');
+var source       = require('vinyl-source-stream');
+var buffer       = require('vinyl-buffer');
+var gutil        = require('gulp-util');
+var uglify       = require('gulp-uglify');
+var streamify    = require('gulp-streamify');
+var sourcemaps   = require('gulp-sourcemaps');
+var concat       = require('gulp-concat');
+var babel        = require('gulp-babel');
+var prod         = gutil.env.prod;
+var gulpSequence = require('gulp-sequence');
 
+var onError = function(err) {
+    console.log(err.message);
+    this.emit('end');
+};
 
 var sassPaths = [
   'bower_components/foundation-sites/scss',
@@ -35,8 +51,36 @@ gulp.task('serve', ['sass'], function() {
 });
 
 gulp.task('copyimg', function() {
-    gulp.src('assets/img/*')
+    gulp.src('assets/img/**/*')
         .pipe(imagemin())
-        .pipe(gulp.dest('dist/img'));
+        .pipe(gulp.dest('dist/img/'));
 });
-gulp.task('default', ['serve', 'copyimg'] );
+
+// bundling js with browserify and watchify
+var b = watchify(browserify('./assets/js/app', {
+    cache: {},
+    packageCache: {},
+    fullPaths: true
+}));
+
+gulp.task('js', bundle);
+b.on('update', bundle);
+b.on('log', gutil.log);
+
+function bundle() {
+    return b.bundle()
+        .on('error', onError)
+        .pipe(source('bundle.js'))
+        .pipe(buffer())
+        .pipe(sourcemaps.init())
+        .pipe(babel({
+            presets: ['es2015']
+        }))
+        .pipe(concat('bundle.js'))
+        .pipe(sourcemaps.write('.'))
+        .pipe(prod ? streamify(uglify()) : gutil.noop())
+        .pipe(gulp.dest('./dist/js'))
+        .pipe(browserSync.stream());
+}
+
+gulp.task('default', gulpSequence(['copyimg','sass', 'js'], 'serve'));
