f475301ed10c1e1c7de489a6e4ef2e22b9cab265
opensource1030 <opensource1030@gmail.com>
2016-08-16 19:29:58 +0545

    JRA CP-1452 #comment support form in progress

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,10 @@
 'use strict';
 var $ = require('jquery');
-var foundation = require('../vendor/foundation-sites/dist/foundation.js');
-var equalizer = require('../vendor/foundation-sites/dist/plugins/foundation.equalizer.js');
+var foundation = require('../vendor/foundation-sites/dist/foundation.min.js');
+require('../vendor/foundation-sites/dist/plugins/foundation.equalizer.js');
+require('../vendor/foundation-sites/dist/plugins/foundation.tooltip.js');
 
 $(document).foundation();
-
 
 
 
