574da474c89503492db367537819d4a1057897c6
opensource1030 <opensource1030@gmail.com>
2016-08-29 12:28:33 -0400

    updates deploy messaging

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,40 +3,13 @@ set :application, 'clean-frontend'
 set :repo_url, 'git@github.com:WirelessAnalytics/clean-frontend.git'
 
 # Slack Notifications
-set :slack_team, "wirelessanalytics"
-set :slack_webhook, "https://hooks.slack.com/services/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"
-set :slack_token, "xoxp-2153660998-7383443462-7449051621-4e69f4"
-set :slack_channel,'#dev-lobby'
-set :slack_icon_emoji, ':rocket:'
-set :slack_username, 'Deployment'
-set :slack_revision, -> { `git rev-parse origin/#{fetch(:branch)}`.strip! }
-set :slack_msg_updated, nil
-set :slack_fallback_updated, "#{fetch(:slack_deploy_user)} deployed #{fetch(:application)} on #{fetch(:stage)}"
-set :slack_fields_updated,
-  [
-    {
-      title: "CLEAN FRONTEND",
-      value: "<https://github.com/WirelessAnalytics/#{fetch(:application)}|#{fetch(:application)}>",
-      short: true
-    },
-    {
-      title: "Environment",
-      value: fetch(:stage),
-      short: true
-    },
-    {
-      title: "Deployer",
-      value: fetch(:slack_deploy_user),
-      short: true
-    },
-    {
-      title: "Revision",
-      value: "<https://github.com/WirelessAnalytics/#{fetch(:application)}/commit/#{fetch(:slack_revision)}|#{fetch(:slack_revision)[0..6]}>",
-      short: true
-    }
-  ]
-
-
+set :slackistrano, {
+    klass: Slackistrano::CustomMessaging,
+    team: "wirelessanalytics",
+    webhook: "https://hooks.slack.com/services/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",
+    token: "xoxp-2153660998-7383443462-7449051621-4e69f4",
+    channel:'#dev-lobby'
+}
 
 # Compile
 namespace :ops do
@@ -46,7 +19,7 @@ namespace :ops do
         on roles(:app), in: :sequence, wait: 1 do
             execute "cd #{release_path} && npm install"
             execute "cd #{release_path} && bower install"
-            execute "cd #{release_path} && gulp build"
+            execute "cd #{release_path} && gulp"
         end
     end
 
