27476415ad6bfb962f56728891be2e269f5a5093
opensource1030 <opensource1030@gmail.com>
2016-09-22 18:16:08 +0200

    #CP-1468 table prices 1.0

--- app/components/views/Device.vue
+++ app/components/views/Device.vue
@@ -3,6 +3,8 @@
   <div class="full-height row">
   <div id="device">
 <div class="header"></div>
+
+
 <div class="expanded row">
 
      <div class="small-12 columns titles" >
@@ -20,13 +22,15 @@
 
 
     <ul  class="acordeon" data-accordion data-allow-all-closed="true" v-f-accordion>
-      <li class="acordeon-item" data-accordion-item  v-f-accordion>
-        <a href="#" class="accordion-title">Device Overview</a>
+      <li class="acordeon-item  is-active" data-accordion-item  v-f-accordion>
+        <a href="#" class="accordion-title"  @click="showFalse()" >Device Overview</a>
         <div class="accordion-content overview"  data-tab-content   v-f-accordion>
           <div class="row">
 <div class="small-2 columns">
-            <img class="phoneImg" src="http://apple.universia.es/wp-content/uploads/2014/09/iPhone-6-oro-555-741.jpg" alt="Photo Iphone 6" />
-              <a class="button large">Upload photo</a>
+            <img class="phoneImg" :src="image" alt="Photo Iphone 6" />
+
+          <label for="FileUpload" class="button large" >Upload File</label>
+<input type="file" id="FileUpload" @change="onFileChange"  class="show-for-sr">
           </div>
           <div class="small-10 columns">
             <label>Tecnical Information
@@ -39,86 +43,84 @@
 
       </li>
 
-      <li class="acordeon-item " data-accordion-item  v-f-accordion>
-        <a href="#" class="accordion-title">Modifications</a>
+      <li class="acordeon-item  " data-accordion-item  v-f-accordion>
+        <a href="#" class="accordion-title" @click="showFalse()"  >Modifications</a>
         <div class="accordion-content  modifications"  data-tab-content   v-f-accordion>
           <div class="row">
+            <div v-if="error" v-show="error">
+          <div   class="is-error callout" data-closable >
+            <div class="container">
+              <h5>{{error}}</h5>
+            </div>
+
+          </div>
+            </div>
             <div class="small-4 columns">
               <div class="row">
+
+
                 <div class="small-6 columns">
-                        <label>Capacity
-                  <input type="text" placeholder="Custom">
+
+                        <label style=" font-weight: bold;" >Capacity
+                  <input type="text" :value=""  v-model="gigas" placeholder="Custom">
                   </label>
                 </div>
                 <div class="small-6 columns">
-                  <a  class="button tiny">Add</a>
+                  <a   @click="capacit()"class="button tiny">Add</a>
                 </div>
                 </div>
 
-                <div class="checkbox">
-                       <label>
-                         <input type="checkbox"  value="32gb" id="32gb" v-model="capacity" checked>
+                <div class="checkbox" v-for="modi in json.modifications.capacity "  track-by="$index">
+                       <label  >
+                         <input type="checkbox"  :value="modi" id="modi" v-model="capacity" >
                          <span class="custom-checkbox"><i class="icon-check"></i></span>
-                         32gb
+                         {{modi}}
                        </label>
                      </div>
-                     <div class="checkbox">
-                            <label>
-                              <input type="checkbox" value="64gb" id="64gb" v-model="capacity"  checked>
-                              <span class="custom-checkbox"><i class="icon-check"></i></span>
-                              64gb
-                            </label>
-                          </div>
+
               </div>
               <div class="small-4 columns end">
           <div class="row">
             <div class="small-6 columns">
-                    <label>Style
-              <input type="text" placeholder="Custom">
+                    <label style=" font-weight: bold;" >Style
+              <input type="text" :value="" v-model="color" placeholder="Custom">
               </label>
             </div>
             <div class="small-6 columns">
-              <a  class="button tiny">Add</a>
+              <a  @click="colors()" class="button tiny">Add</a>
             </div>
             </div>
-            <div class="checkbox">
+            <div class="checkbox" v-for="styles in json.modifications.style "  track-by="$index" >
                    <label>
-                     <input type="checkbox"   value="black" id="black" v-model="style" checked>
+                     <input type="checkbox"   :value="styles" id="styles" v-model="style" >
                      <span class="custom-checkbox"><i class="icon-check"></i></span>
-                    Black
+                    {{styles}}
                    </label>
                  </div>
-                 <div class="checkbox">
-                        <label>
-                          <input type="checkbox" value="yelow"  id="yellow" v-model="style" checked>
-                          <span class="custom-checkbox"><i class="icon-check"></i></span>
-                          Yelow
-                        </label>
-                      </div>
+
 
                 </div>
         </div>
         </div>
       </li>
       <li class="acordeon-item " data-accordion-item  v-f-accordion>
-        <a href="#" class="accordion-title">Carriers</a>
+        <a href="#" class="accordion-title" @click="showFalse()" >Carriers</a>
         <div class="accordion-content carriers"  data-tab-content   v-f-accordion>
             <div class="imagescheck">
+              <div class="crop"  v-for="carrier in json.carriers"  >
 
-          <label :style="{ backgroundImage: 'url(' + image + ')',width:'120px',height:'120px' }"  >
-            <input type="checkbox" value="ac"   :style="{ backgroundImage: 'url(' + image + ')',width:'120px',height:'120px' }" name="" id="ac" v-model="carriers"   >
-
-          </label>
-          <label :style="{ backgroundImage: 'url(' + image + ')',width:'120px',height:'120px' }"  >
-            <input type="checkbox" value="Mike"  :style="{ backgroundImage: 'url(' + image + ')',width:'120px',height:'120px' }" name="" id="mike" v-model="carriers"   >
+          <label   class="static" :class="{ 'active':carrier.va }"  :style="{ backgroundImage: 'url(' + carrier.image + ')' }"  >
+            <input type="checkbox"  @click="t($index)" :value="carrier.name"  :style="{ backgroundImage: 'url(' + carrier.image + ')' }" name="" id="carrier.name" v-model="carrierss"   >
 
           </label>
+        </div>
+
         </div>
 
         </div>
       </li>
       <li class="acordeon-item " data-accordion-item  v-f-accordion>
-        <a href="#" class="accordion-title">Companies</a>
+        <a href="#" class="accordion-title" @click="showFalse()"   >Companies</a>
         <div class="accordion-content companies"  data-tab-content   v-f-accordion>
           <div class="row">
             <div class="small-6 columns find">
@@ -132,61 +134,35 @@
 
         </div>
           <div class="row">
-              <div class="small-2 columns">
-                <div class="checkbox">
-                       <label>
-                         <input type="checkbox" name="" id="checkbox1" checked>
-                         <span class="custom-checkbox"><i class="icon-check"></i></span>
-                         Default
-                       </label>
+              <div class="small-2 columns"  >
+                <div class="checkbox"  v-for="company in json.companies "   >
+                  <label>
+                    <input  @click=""   type="checkbox"   :value="company" id="commpa" v-model="compa"  >
+                    <span class="custom-checkbox"><i class="icon-check"></i></span>
+                   {{company.name}}
+                  </label>
                      </div>
-                     <div class="checkbox">
-                            <label>
-                              <input type="checkbox" name="" id="checkbox1" checked>
-                              <span class="custom-checkbox"><i class="icon-check"></i></span>
-                              Default
-                            </label>
-                          </div>
-                          <div class="checkbox">
-                                 <label>
-                                   <input type="checkbox" name="" id="checkbox1" checked>
-                                   <span class="custom-checkbox"><i class="icon-check"></i></span>
-                                   Default
-                                 </label>
-                               </div>
+
+
+
 
             </div>
 
             <div class="small-2  small-offset-1   columns">
-              <ul>
-              <li>399$</li>
-                <li>499$</li>
-                  <li>599$</li>
-                    <li>600$</li>
-                    <li>999$</li>
-                      <li>999$</li>
+              <ul style=" font-weight: bold;"    >
+              <li v-for="company in json.companies"   >{{company.price}}</li>
+
 
           </ul>
           </div>
               <div class="small-2 columns">
-                <ul>
-                <li>Mateo Bistuer</li>
-                  <li>Mateo Bistuer</li>
-                    <li>Mateo Bistuer</li>
-                      <li>Mateo Bistuer</li>
-                      <li>Mateo Bistuer</li>
-                        <li>Mateo Bistuer</li>
-
+                <ul  >
+                <li v-for="company in json.companies" >{{company.author}}</li>
             </ul>
                   </div>
                   <div class="small-2 columns end">
                     <ul>
-                    <li>666-666-6666</li>
-                      <li>666-666-6666</li>
-                        <li>666-666-6666</li>
-                          <li>666-666-6666</li>
-                          <li>666-666-6666</li>
-                            <li>666-666-6666</li>
+                    <li v-for="company in json.companies"  >{{company.code}}</li>
 
                 </ul>
                       </div>
@@ -195,8 +171,8 @@
         </div>
       </li>
       <li class="acordeon-item prices" data-accordion-item  v-f-accordion>
-       <a  href="#"  class="accordion-title"  >   Prices  </a>
-       <div class="filterprices">
+       <a  href="#"  class="accordion-title" @click="toggle()"  >   Prices  </a>
+       <div class="filterprices" v-show="show">
     <select class="form-control"  >
          <option value="" selected>Capacity</option>
          <option value=""></option>
@@ -215,34 +191,42 @@
         </select>
 
    </div>
-        <div class="accordion-content"  data-tab-content   v-f-accordion>
-          <table>
-
+   <div   class="accordion-content"  data-tab-content   v-f-accordion>
+          <table  >
+            <tbody>
+              <tr class="filter">
+                  <td><div>Retail Price</div></td>
+                <td><div>1 year contact</div></td>
+                <td ><div>2 years contract</div></td>
+                <td  ><div>Pay by own</div></td>
+                <td  ><div></div></td>
+                <td ><div></div></td>
+                <td ><div></div></td>
+                <td  ><div></div></td>
+              </tr>
+            </tbody>
 <tbody>
-  <tr>
-    <td>Content Goes Here</td>
-  <td>Content Goes Here</td>
-    <td>Content Goes Here</td>
-    <td>Content Goes Here</td>
-    <td>Content Goes Here</td>
-  <td>Content Goes Here</td>
-    <td>Content Goes Here</td>
-    <td>Content Goes Here</td>
+  <tr  v-for="price in priceTable" track-by="$index">
+    <td ><div class="price">{{price.price}}</div></td>
+    <td><div class="price">{{price.price}}</div></td>
+    <td><div class="price">{{price.price}}</div></td>
+    <td><div class="price">{{price.price}}</div></td>
+    <td><div class="features">{{price.capacity}}</div></td>
+  <td><div class="features">{{price.style}}</div></td>
+    <td><div class="features">{{price.carrier}}</div></td>
+    <td style=" font-weight: bold;"><div class="features">{{price.name}}</div></td>
   </tr>
 
 </tbody>
+
 </table>
+</div>
 
-
-
-        </div>
       </li>
-
-
 
     </ul>
 
-
+    <a href="#" class="button large" id="button">Save Changes</a>
 
 </div>
 </div>
@@ -251,6 +235,7 @@
 </template>
 <script>
 import Vue from 'vue'
+
 
 Vue.directive('f-accordion', {
 
@@ -280,9 +265,97 @@ export default {
 
 
     },
+    computed: {
+        priceTable: function () {
+
+
+
+if(this.compa.length>0){
+  this.price=[]
+  var style=[];
+   for (let i = 0; i < this.compa.length ; i++) {
+
+           for (var j = 0; j < this.style.length; j++) {
+
+                    this.price.push(this.compa[i]);
+                 this.price[j] = Object.assign({}, this.price[j], { style: this.style[j],capacity:this.capacity[j],carrier:this.carrierss[j]});
+
+
+                             }
+
+                }
+
+                return this.price;
+}
+
+else{
+  return '';
+}
+
+
+    }
+  },
+
     methods: {
+      toggle: function () {
+           this.show = !this.show;
+
+
+
+
+
+
+       },
+       showFalse:function () {
+         this.show=false;
+        },
+
+        onFileChange(e) {
+     var files = e.target.files || e.dataTransfer.files;
+     if (!files.length)
+       return;
+     this.createImage(files[0]);
+   },
+   createImage(file) {
+     var image = new Image();
+     var reader = new FileReader();
+     var vm = this;
+
+     reader.onload = (e) => {
+       vm.image = e.target.result;
+     };
+     reader.readAsDataURL(file);
+   },
+   t:function(index){
+
+
+     this.json.carriers[index].va= !this.json.carriers[index].va;
+
+
+   },
+   capacit:function(){
+        if(this.gigas=="" || this.gigas==null){
+              this.error="Incorrect value Capacity"
+        }
+     else{
+       this.error="";
+     this.json.modifications.capacity.push(this.gigas);
+   }
+
+ },
+  colors:function(){
+    if(this.color=="" || this.color==null){
+          this.error="Incorrect value Style"
+    }
+ else{
+   this.error="";
+ this.json.modifications.style.push(this.color);
+}
+
+  }
 
     },
+
 
 
     data() {
@@ -290,10 +363,76 @@ export default {
       return {
 
 
-             image: "https://fixwoozone-nvitotech1.netdna-ssl.com/wp-content/uploads/2015/08/ATT-logo-.png",
-             carriers:[],
+             image: "http://apple.universia.es/wp-content/uploads/2014/09/iPhone-6-oro-555-741.jpg",
+             carrierss:[],
              style:[],
-             capacity:[]
+             capacity:[],
+             compa:[],
+             price:[],
+             gigas:'',
+             color:'',
+             error:'',
+             show:false,
+             shadow:"initial",
+             json:{
+                modifications:{
+                        capacity:['8gb','16gb','32gb','64gb','128gb'],
+                        style:['white','silver','space grey','rose Gold']
+
+                },
+
+                carriers:[
+                  {
+                name:'at&t',
+                image:"http://apple.universia.es/wp-content/uploads/2014/09/iPhone-6-oro-555-741.jpg",
+                va:false
+              },
+              {
+              name:'Sprint',
+              image:"http://apple.universia.es/wp-content/uploads/2014/09/iPhone-6-oro-555-741.jpg",
+              va:false
+
+            },
+            {
+              name:'t-Mobile',
+              image:"http://apple.universia.es/wp-content/uploads/2014/09/iPhone-6-oro-555-741.jpg",
+              va:false
+          },
+          {
+            name:'Verizon',
+            image:"http://apple.universia.es/wp-content/uploads/2014/09/iPhone-6-oro-555-741.jpg",
+            va:false
+          }
+          ],
+                companies:[
+                  {
+                    name:'thermotronic',
+                    price:'$538',
+                    author:'Antonio Olson',
+                    code:'880-012-5128',
+
+                  },
+                  {
+                    name:'Cocoa',
+                    price:'$538',
+                    author:'Alfred Graves',
+                    code:'880-012-5128',
+
+                  },
+                  {
+                    name:'Pepsi',
+                    price:'$538',
+                    author:'LydIA fRANK',
+                    code:'880-012-5128',
+
+                  }
+
+                ]
+
+             },
+             jsonPrices:{}
+
+
 
 
 
