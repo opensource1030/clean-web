038ebbfd5017602062e4f401ea1358879aa38cc9
opensource1030 <opensource1030@gmail.com>
2016-10-26 18:35:58 -0400

    Adds deploy on successful build (to development for now)

--- config/dev.env.js
+++ config/dev.env.js
@@ -1,10 +1,10 @@
-var merge = require('webpack-merge')
-var prodEnv = require('./prod.env')
+var merge = require('webpack-merge');
+var prodEnv = require('./prod.env');
 
 module.exports = merge(prodEnv, {
-    NODE_ENV: '"development"',
-    URL_API: '"http://lfce85j83fdtoxhkw.stoplight-proxy.io/"', // stoplight.io proxy to Dev
-    URL: '"http://localhost:8080"',
-    CLIENT_ID: '"g73hhd8j3bhcuhdbbs88e4wd"',
-    CLIENT_SECRET: '"786wndkd8iu4nn49ixjndfodsde33"'
-})
+  NODE_ENV: '"development"',
+  URL_API: '"http://lfce85j83fdtoxhkw.stoplight-proxy.io/"', // stoplight.io proxy to Dev
+  URL: '"http://localhost:8080"',
+  CLIENT_ID: '"g73hhd8j3bhcuhdbbs88e4wd"',
+  CLIENT_SECRET: '"786wndkd8iu4nn49ixjndfodsde33"',
+});
--- config/prod.env.js
+++ config/prod.env.js
@@ -1,10 +1,12 @@
 module.exports = {
   NODE_ENV: '"production"',
   URL_API: '"http://dev.api.wirelessanalytics.com/"',
-  URL:'"http://dev.wirelessanalytics.com"',
-  CLIENT_ID:'"g73hhd8j3bhcuhdbbs88e4wd"',
-  CLIENT_SECRET:'"786wndkd8iu4nn49ixjndfodsde33"'
-}
+  URL: '"http://dev.wirelessanalytics.com"',
+  CLIENT_ID: '"g73hhd8j3bhcuhdbbs88e4wd"',
+  CLIENT_SECRET: '"786wndkd8iu4nn49ixjndfodsde33"',
+};
+
+
 // module.exports = {
 //   NODE_ENV: '"production"',
 //   URL_API: '"https://api.wirelessanalytics.com"',
