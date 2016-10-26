478254c9e27ad7184a220e4d79135be6bcfb7a7e
opensource1030 <opensource1030@gmail.com>
2016-10-26 15:24:31 -0400

    Adds deployment configuration via capistrano - CP-XXX This add deploying via capistrano/ruby. See http://capistranorb.com/

---
+++ config/deploy.rb
@@ -0,0 +1,25 @@
+# config valid only for current version of Capistrano
+set :application, 'clean-frontend'
+set :repo_url, 'git@github.com:WirelessAnalytics/clean-frontend.git'
+
+# Slack Notifications
+set :slackistrano, {
+    klass: Slackistrano::CustomMessaging,
+    team: "wirelessanalytics",
+    webhook: "https://hooks.slack.com/services/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",
+    token: "xoxp-2153660998-7383443462-7449051621-4e69f4",
+    channel:'#dev-lobby'
+}
+
+# Compile
+namespace :ops do
+
+  desc 'Assets Compile'
+    task :asset_compile do
+        on roles(:app), in: :sequence, wait: 1 do
+            execute "cd #{release_path} && npm install"
+            execute "cd #{release_path} && npm run build"
+        end
+    end
+
+end
