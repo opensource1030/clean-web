59603ccdf6870fade9c025852a4246bae8996c97
opensource1030 <opensource1030@gmail.com>
2016-11-08 21:27:43 +0545

    chart api call Inprogress

--- src/components/PieChart.js
+++ src/components/PieChart.js
@@ -1,16 +1,53 @@
-import { Pie, reactiveProp } from 'vue-chartjs'
+import Vue from 'vue'
+import auth from './../api/auth'
+import { Pie } from 'vue-chartjs'
 var {
-  Store,
+    Store,
 } = require('yayson')();
 var store = new Store();
-
 export default Pie.extend({
-  mixins: [reactiveProp],
 
-  props: ['chartData', 'options'],
+    mounted () {
+        this.renderChart(this.pieData, this.options_3);
+    },
+    data () {
+        return {
+            labels: ['Usage Charge', 'Service Plan Charges','Other Charges'], // I want to put data from api call
+            data: [1, 15, 27.4], // I want to put data from api call
+            options: {
+                segmentShowStroke: false
+            },
+            backgroundColor: [
+                '#1fc8db',
+                '#fce473',
+                '#42afe3',
+                '#ed6c63',
+                '#97cd76'
+            ],
+            options_3: {
+                tooltips: {
+                    mode: 'label'
+                },
+                legend :{
+                    position: 'bottom'
+                }
+            }
+        }
+    },
+    computed: {
+        pieData () {
+            let data = {
+                labels: this.labels
+            }
+             data.datasets= [{
+                    data: this.data,
+                    backgroundColor: this.backgroundColor
+                }]
+            return data;
 
-  mounted () {
-    // console.log('child mounted', this.chartData);
-    this.renderChart(this.chartData, this.options);
-  },
+        }
+    },
+    methods: {
+
+    }
 })
