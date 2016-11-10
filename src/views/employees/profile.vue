d102ea3682475eeb50676f1e2966b5791a4bc12b
opensource1030 <opensource1030@gmail.com>
2016-11-10 15:29:53 +0100

    CP-1520 add style profile employee

--- src/views/employees/profile.vue
+++ src/views/employees/profile.vue
@@ -60,7 +60,160 @@
                 <li class="acordeon-item  " >
                   <a  class="accordion-title"   >My Services</a>
                   <div class="accordion-content" >
-                    
+
+                    <div class="row">
+                          <div class="small-3 columns">
+                            <div >
+                                  <img   src="./../../assets/wa-logo.png" aria-hidden=true alt="Photo Iphone 6" width="200px" height="800px" class="custom-thumbnail-class" />
+</div>
+
+                          </div>
+                          <div class="small-3 columns">
+                            <div class="row">
+                                <h6>AT&T</h6>
+
+                            </div>
+                            <div class="row">
+                            <ul>
+                              <li><label>pooled Domestic Voice Plan</label></li>
+                                <li><label>pooled Domestic Voice Plan</label></li>
+                                  <li><label>pooled Domestic Voice Plan</label></li>
+                              <ul>
+                            </div>
+
+
+                          </div>
+                          <div class="small-3 columns">
+                            <div class="row">
+
+                                <h6>$64.09 </h6> <label> monthly</label>
+
+                            </div>
+                            <div class="row">
+                            <ul>
+                              <li><label>30.00</label></li>
+                                <li><label>5.99</label></li>
+                                  <li><label>23.10</label></li>
+                              <ul>
+                            </div>
+
+
+
+
+                          </div>
+                          <div class="small-3 columns">
+                                  <a class="button expand" @click="save()" >Change Services</a>
+                          </div>
+
+
+                    </div>
+                    <div class="row">
+                          <div class="small-3 columns">
+                            <div >
+                                  <img   src="./../../assets/wa-logo.png" aria-hidden=true alt="Photo Iphone 6" width="200px" height="800px" class="custom-thumbnail-class" />
+</div>
+
+                          </div>
+                          <div class="small-3 columns">
+                            <div class="row">
+                                <h6>AT&T</h6>
+
+                            </div>
+                            <div class="row">
+                            <ul>
+                              <li><label>pooled Domestic Voice Plan</label></li>
+                                <li><label>pooled Domestic Voice Plan</label></li>
+                                  <li><label>pooled Domestic Voice Plan</label></li>
+                              <ul>
+                            </div>
+
+
+                          </div>
+                          <div class="small-3 columns">
+                            <div class="row">
+
+                                <h6>$64.09 </h6> <label> monthly</label>
+
+                            </div>
+                            <div class="row">
+                            <ul>
+                              <li><label>30.00</label></li>
+                                <li><label>5.99</label></li>
+                                  <li><label>23.10</label></li>
+                              <ul>
+                            </div>
+
+
+
+
+                          </div>
+                          <div class="small-3 columns">
+                            <div class="row">
+                                  <a class="button expand" @click="save()" >Repair Device</a>
+                                </div>
+                                <div class="row">
+                                      <a class="button expand" @click="save()" >Upgrade Device</a>
+                                      <label>Upgrade will be available in 3 months</label>
+                                    </div>
+
+
+                          </div>
+
+
+
+                    </div>
+                    <div class="row">
+                          <div class="small-3 columns">
+                            <div >
+                                  <img   src="./../../assets/wa-logo.png" aria-hidden=true alt="Photo Iphone 6" width="200px" height="800px" class="custom-thumbnail-class" />
+</div>
+
+                          </div>
+                          <div class="small-3 columns">
+                            <div class="row">
+                                <h6>AT&T</h6>
+
+                            </div>
+                            <div class="row">
+                          <div class="radio">
+                                  <input type="radio"  value="Red" id="subsidedDevice" required><label for="subsidedDevice">Subsided Device</label>
+                                  <span  class="sub">choose a device from a list</span>
+                          </div>
+                          <div class="radio">
+                                  <input type="radio" value="Red" id="payByYourself" required><label for="payByYourself">Pay by Yourself</label>
+                                          <span class="sub" >choose any device and pay by yourself</span>
+                          </div>
+                          <div class="radio">
+                                  <input type="radio"  value="Red" id="accesories" required><label for="accesories">Accesories</label>
+                                        <span class="sub">headphones,chargers,bags</span>
+
+                          </div>
+
+                            </div>
+                            <div class="radio">
+                                    <input type="radio"  value="Red" id="customRequest" required><label for="customRequest">Custom Request</label>
+                                            <span class="sub">something special</span>
+                            </div>
+
+
+
+                          </div>
+
+                          <div class="small-3 columns">
+                            <div class="row">
+                                  <a class="button expand" @click="save()" >Request Device</a>
+                                </div>
+
+
+
+                          </div>
+
+
+
+                    </div>
+
+
+
 
 
 
