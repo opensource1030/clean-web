23d010360c9d45d5a720656894ca69f1a0e63393
opensource1030 <opensource1030@gmail.com>
2016-11-10 10:53:05 +0100

    Api call issue with charts

---
+++ src/components/Trendchart.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <div class="large-6 columns">
+      <div class="grid-box" data-mh="box2">
+        <header class="box-heading">
+          <h2>Trend By Category- Custom</h2>
+        </header>
+        <div class="box-content coming-soon">
+          <trend-chart  :datas="callback" :height="200" > </trend-chart>
+        </div>
+      </div>
+    </div>
+    </div>
+</template>
+
+<script>
+  import PieData from './../api/piedata'
+  import Vue from 'vue'
+
+
+  import TrendChart from './TrendChart'
+  export default {
+    components: { TrendChart },
+
+    data(){
+        return{
+          data:null
+        }
+
+    },
+    methods:{
+          callback(){
+                //API CALL IN THE FILE ./../api/piedata 
+              //  PieData.get(this,this.data)
+       return  this.data=[
+        [25, 40, 50],
+        [28, 48, 40],
+        [10, 55, 30]
+      ];
+
+
+          }
+
+    }
+
+  }
+</script>
