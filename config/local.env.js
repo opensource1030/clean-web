edfa7ec4810cbce0cc9ffb1506b0b0e25969b840
opensource1030 <opensource1030@gmail.com>
2016-11-10 16:19:46 +0100

    tipo url with api

--- config/local.env.js
+++ config/local.env.js
@@ -1,10 +1,8 @@
-var merge = require('webpack-merge')
-var Env = require('./local.env')
 
-module.exports = merge(Env, {
+module.exports =  {
   NODE_ENV: '"local"',
   URL_API: '"http://lfce85j83fdtoxhkw.stoplight-proxy.io/"', // stoplight.io mock server
   URL:'"http://localhost:8080"',
   CLIENT_ID:'"g73hhd8j3bhcuhdbbs88e4wd"',
   CLIENT_SECRET:'"786wndkd8iu4nn49ixjndfodsde33"'
-})
+}
