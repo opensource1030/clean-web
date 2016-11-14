59f7e321ef19e237ddeeefb562feb6f9db86f598
opensource1030 <opensource1030@gmail.com>
2016-11-14 16:22:54 -0500

    updates the address without the proxies

--- config/local.env.js
+++ config/local.env.js
@@ -2,7 +2,7 @@
 module.exports =  {
   NODE_ENV: '"local"',
   // stoplight.io Mock Server
-  URL_API: '"http://lfce85j83fdtoxhkw-mock.stoplight-proxy.io/"', 
+  URL_API: '"http://dev.wirelessanalytics.com/"', 
   URL:'"http://localhost:8080"',
   CLIENT_ID:'"g73hhd8j3bhcuhdbbs88e4wd"',
   CLIENT_SECRET:'"786wndkd8iu4nn49ixjndfodsde33"'
