328f2aef52128a47571dbdf5b2d45b49ccdfb4ca
opensource1030 <opensource1030@gmail.com>
2016-11-15 13:38:42 -0500

    Updates the companies resource

--- src/components/ChargeInfo.vue
+++ src/components/ChargeInfo.vue
@@ -90,7 +90,7 @@ export default {
     name: "ChargeInfo",
   created(){
 
-    this.$http.get(process.env.URL_API+'/users/'+localStorage.userId+'?include=company,allocations&filter[allocations.billMonth]=[company.currentBillMonth]').then((response) => {
+    this.$http.get(process.env.URL_API+'/users/'+localStorage.userId+'?include=companies,allocations&filter[allocations.billMonth]=[companies.currentBillMonth]').then((response) => {
       var event = store.sync(response.data);
       this.allocation= event;
 
