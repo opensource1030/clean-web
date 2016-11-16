ca46504bf8e4407d1a0a2ec2d66488158476d176
opensource1030 <opensource1030@gmail.com>
2016-11-16 17:25:27 +0100

    CP-1630 Routes sidemenu

--- config/local.env.js
+++ config/local.env.js
@@ -3,10 +3,14 @@ module.exports =  {
   NODE_ENV: '"local"',
 <<<<<<< HEAD
   // stoplight.io Mock Server
+<<<<<<< HEAD
   URL_API: '"http://dev.wirelessanalytics.com/"',
 =======
   URL_API: '"http://lfce85j83fdtoxhkw.stoplight-proxy.io/"', // stoplight.io mock server
 >>>>>>> CP-1594 Slicing services
+=======
+  URL_API: '"http://clean.api/"',
+>>>>>>> CP-1630 Routes sidemenu
   URL:'"http://localhost:8080"',
   CLIENT_ID:'"2"',
   CLIENT_SECRET:'"ab9QdKGBXZmZn50aPlf4bLlJtC4BJJNC0M99i7B7"'
