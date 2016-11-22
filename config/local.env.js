517d500beae75b4a16f42cbe13f24561117d4f53
opensource1030 <opensource1030@gmail.com>
2016-11-21 19:19:03 -0500

    Sets config to pull in the needed value

--- config/local.env.js
+++ config/local.env.js
@@ -1,8 +1,7 @@
-
 module.exports =  {
   NODE_ENV: '"local"',
   // stoplight.io Mock Server
-  URL_API: '"http://lfce85j83fdtoxhkw-mock.stoplight-proxy.io/"',
+  URL_API: '"http://lfce85j83fdtoxhkw-mock.stoplight-proxy.io"',
   URL:'"http://localhost:8080"',
   CLIENT_ID:'"g73hhd8j3bhcuhdbbs88e4wd"',
   CLIENT_SECRET:'"786wndkd8iu4nn49ixjndfodsde33"'
