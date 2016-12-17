9770021e1ef5b1f0e188412c8e945442ffd155a4
opensource1030 <opensource1030@gmail.com>
2016-12-18 00:06:00 +0800

    integrate api to trend-chart

--- src/api/piedata.js
+++ src/api/piedata.js
@@ -11,27 +11,40 @@ var store = new Store();
 
 export default {
   /*-------------------------------update device-----------------*/
+  lineChart: [],
+  chart: [],
 
-  get(context,data){
-
-
-    context.$http.get(process.env.URL_API+'/users/'+localStorage.userId,{
-        params: {
-            include: 'allocations',
-        },
+  getCharts(context) {
+    context.$http.get(process.env.URL_API+'/users/' + localStorage.userId, {
+      params: {
+        include: 'allocations',
+      },
     }).then((response) => {
-        var  test = store.sync(response.data);
-        console.log(test);
-        var allocation = test.allocations;
+      var event = store.sync(response.data);
 
-        var data = Object.keys(allocation).map( function (k) {return allocation[k];} );
-        console.log(data);
+      // this.lineChart = [];
+      // for (let allocation of event.allocations ) {
+      //   this.lineChart.push([allocation.usage_charge, allocation.other_charge, allocation.service_plan_charge]);
+      // }
 
+      var result = _.chain(event.allocations).groupBy('bill_month').map((o, key) => {
+        return {
+          'usage_charge': Math.round(_.sumBy(o, 'usage_charge') * 100) / 100,
+          'service_plan_charge': Math.round(_.sumBy(o, 'service_plan_charge') * 100) / 100,
+          'other_charge': Math.round(_.sumBy(o, 'other_charge') * 100) / 100,
+          'bill_month': key
+        };
+      }).orderBy('bill_month').value();
 
+      // console.log(this.lineChart);
+      // context.data = this.lineChart;
+
+      context.data = [
+        _.map(result, 'usage_charge'),
+        _.map(result, 'service_plan_charge'),
+        _.map(result, 'other_charge'),
+      ];
     }, (response) => {
-
     });
-
   }
-
 };
