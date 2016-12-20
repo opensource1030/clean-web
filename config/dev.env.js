667a6262d37476e5b266c1e2d808b07604c096ea
opensource1030 <opensource1030@gmail.com>
2016-12-20 10:21:36 +0100

    CP-1468

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,5 +3,21 @@ module.exports = {
   URL_API: '"http://dev.api.wirelessanalytics.com"',
   URL: '"http://dev.wirelessanalytics.com"',
   CLIENT_ID: '"2"',
-  CLIENT_SECRET: '"ab9QdKGBXZmZn50aPlf4bLlJtC4BJJNC0M99i7B7"'
+  CLIENT_SECRET: '"ab9QdKGBXZmZn50aPlf4bLlJtC4BJJNC0M99i7B7"',
+  FEATURES: {
+    "ManageDevices": {
+        enabled: false,
+        users: {
+            "user@mail.com": true,
+            "*@email.com": true
+        }
+    },
+    "ManageServices": {
+        enabled: false,
+        users: {
+            "user@mail.com": true,
+            "*@wa.com": true
+        }
+    }
+}
 };
