6399315c2eaf9fb7c37d65916f349afe3ae62e6a
opensource1030 <opensource1030@gmail.com>
2016-12-21 22:35:49 +0800

    fix pie chart

--- src/components/PieChart.js
+++ src/components/PieChart.js
@@ -1,11 +1,11 @@
-import { Pie, reactiveProp } from 'vue-chartjs'
+import { Pie, mixins } from 'vue-chartjs'
 var {
   Store,
 } = require('yayson')();
 var store = new Store();
 
 export default Pie.extend({
-  mixins: [reactiveProp],
+  mixins: [mixins.reactiveProp],
 
   props: ['chartData', 'options'],
 
