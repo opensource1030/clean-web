8eb05b3309c2b795651cb8581972ef79695964c8
opensource1030 <opensource1030@gmail.com>
2016-12-30 02:09:59 +0545

    CP-1457 updated some UI of support request form

--- config/local.env.js
+++ config/local.env.js
@@ -2,7 +2,7 @@
 module.exports = {
   NODE_ENV: '"local"',
   // stoplight.io Mock Server
-  URL_API: '"http://dev.api.wirelessanalytics.com"',
+  URL_API: '"http://lfce85j83fdtoxhkw-mock.stoplight-proxy.io"',
   URL: '"http://localhost:8080"',
   CLIENT_ID: '"2"',
   CLIENT_SECRET: '"ab9QdKGBXZmZn50aPlf4bLlJtC4BJJNC0M99i7B7"',
