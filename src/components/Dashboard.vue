9010b7508a9596d277b9329d29787efd484b48b1
opensource1030 <opensource1030@gmail.com>
2017-01-18 05:34:35 +0800

    CP-1599 add tabs to chart

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -126,8 +126,13 @@
         </form>
       </div>
       <div class="clearfix"></div>
+
+      <div>
         <piechart :data="piechartData"></piechart>
-        <trendchart></trendchart>
+        <trendchart :data="trendchartData"></trendchart>
+        <div class="clearfix"></div>
+      </div>
+      <div class="clearfix"></div>
     </div>
     <spent-Info></spent-Info>
   </div>
@@ -204,9 +209,10 @@
           }, 2000);
         });
       });
+
       this.$http.get(process.env.URL_API + '/users/'+ localStorage.userId +'?include=companies,companies.contents,companies.currentBillMonths,allocations&filter[allocations.billMonth]=[currentBillMonths.last:3]', {
       }).then((response) => {
-
+        // console.log('response', response.data);
         var event = store.sync(response.data);
         if (event.companies.length>0) {
           var clientdata = event.companies[0].contents[0].content;
@@ -226,7 +232,7 @@
                 account: "JPYPKy",
                 enabled : true
               };
-              Headway.init(config);
+              // Headway.init(config);
               var $select = $('#support-form .user-actions'), $images = $('.mix');
               $select.on('change', function () {
                 var value = '.' + $(this).val();
@@ -256,66 +262,79 @@
               $(document).keyup(function (e) {
                 if (e.keyCode == 27) $('#btn-close').click();
               });
-                        $("#support-form").validate({
-                            rules: {
-                                "description": {
-                                    required: true,
-                                    minlength: 8
-                                }
-                            },
-                            submitHandler: function (form) {
-                                var form = $('#support-form');
-                                var $modal = $('#modal');
-                                var company = "wirelessanalytics";
-                                var key = "PMf04HTtZ7dNDIS2gmQCUWWRw0IwaHvdoa3MYQ6Fg6f23s8zrr";
-                                var json = {
-                                     "assignedTo":59063,
-                                     "inboxId":1778,
-                                    "subject": $('#subject').val(),
-                                    "tags" : [ $('#tags').val() ],
-                                    "customerEmail": $('#recipient_email').val(),
-                                    "customerMobileNumber" : $('#recipient_mobilenumber').val(),
-                                    "customerPhoneNumber" : $('#recipient_phonenumber').val(),
-                                    "message":$('#description').val() + ' <br/><br/>' + '<hr/>' + '<strong>Prefers:'+ ' ' + $('input[name=method]:checked', '#support-form').val() + "</strong>",
-                                    "source": "clean-dashboard",
-                                    "status" : "active",
-                                    "priority" : $('input[name=priority]:checked', '#support-form').val()
-                                };
-                                $.ajax({
-                                    type: "POST",
-                                    url: "https://" + company + ".teamwork.com/desk/v1/tickets.json",
-                                    headers: {"Authorization": "BASIC " + window.btoa(key + ":xxx")},
-                                    data : JSON.stringify(json),
-                                    processData: false,
-                                    contentType: "application/json; charset=UTF-8",
-                                    success: function(msg){
-                                        $modal.html('');
-                                        $modal.removeClass('is-error').addClass('is-success').append("<h4>Request sent succefully </h4>" +"<button data-close='' aria-label='Close Accessible Modal' type='button' class='close-button'><span aria-hidden='true'>×</span></button>").foundation('open');
-                                        $('#support-form')[0].reset();
-                                    },
-                                    error: function(XMLHttpRequest, textStatus, errorThrown) {
-                                        $('#modal').html('');
-                                        $modal.removeClass('is-success').addClass('is-error').append("<h4>"+"Status: " + textStatus+"</h4>"+"<p>"+"Error: " + "Please fill all the required fields with appropriate value" + "</p>" +"<button data-close='' aria-label='Close Accessible Modal' type='button' class='close-button'><span aria-hidden='true'>×</span></button>").foundation('open');
-                                    }
-                                });
 
-                            }
-                        });
-                    },300);
-                });
-            }
-          if (event.allocations && event.allocations.length > 0) {
-              this.$set(this, 'piechartData', [
-                  _.sumBy(event.allocations, 'data_category'),
-                  _.sumBy(event.allocations, 'other_category'),
-                  _.sumBy(event.allocations, 'unknown_category'),
-                  _.sumBy(event.allocations, 'voice_category'),
-              ]);
+              $("#support-form").validate({
+                  rules: {
+                      "description": {
+                          required: true,
+                          minlength: 8
+                      }
+                  },
+                  submitHandler: function (form) {
+                      var form = $('#support-form');
+                      var $modal = $('#modal');
+                      var company = "wirelessanalytics";
+                      var key = "PMf04HTtZ7dNDIS2gmQCUWWRw0IwaHvdoa3MYQ6Fg6f23s8zrr";
+                      var json = {
+                           "assignedTo":59063,
+                           "inboxId":1778,
+                          "subject": $('#subject').val(),
+                          "customerEmail": $('#recipient_email').val(),
+                          "customerMobileNumber" : $('#recipient_mobilenumber').val(),
+                          "customerPhoneNumber" : $('#recipient_phonenumber').val(),
+                          "message":$('#description').val() + ' ' + ' Preferred:'+ ' ' + $('input[name=method]:checked', '#support-form').val(),
+                          "source": "clean-dashboard",
+                          "status" : "active",
+                          "tags[]" : $('#tags').val(),
+                          "priority" : $('input[name=priority]:checked', '#support-form').val()
+                      };
+                      $.ajax({
+                          type: "POST",
+                          url: "https://" + company + ".teamwork.com/desk/v1/tickets.json",
+                          headers: {"Authorization": "BASIC " + window.btoa(key + ":xxx")},
+                          data : JSON.stringify(json),
+                          processData: false,
+                          contentType: "application/json; charset=UTF-8",
+                          success: function(){
+                              $modal.html('');
+                              $modal.removeClass('is-error').addClass('is-success').append("<h4>Request sent succefully </h4>" +"<button data-close='' aria-label='Close Accessible Modal' type='button' class='close-button'><span aria-hidden='true'>×</span></button>").foundation('open');
+                              $('#support-form')[0].reset();
+                          },
+                          error: function(XMLHttpRequest, textStatus, errorThrown) {
+                              $('#modal').html('');
+                              $modal.removeClass('is-success').addClass('is-error').append("<h4>"+"Status: " + textStatus+"</h4>"+"<p>"+"Error: " + "Please fill all the required fields with appropriate value" + "</p>" +"<button data-close='' aria-label='Close Accessible Modal' type='button' class='close-button'><span aria-hidden='true'>×</span></button>").foundation('open');
+                          }
+                      });
+                  }
+              });
+            },300);
+          });
+        }
 
-              // console.log(event.allocations);
-              // console.log(this.piechartData);
-          }
-        });
+        // console.log('trendchartData', event.allocations);
+        if (event.allocations && event.allocations.length > 0) {
+          // this.$set(this, 'piechartData', [0, 0, 0, 0]);
+
+          // this.$set(this, 'piechartData', [
+          //   _.sumBy(event.allocations, 'service_plan_charge'),
+          //   _.sumBy(event.allocations, 'other_category') + _.sumBy(event.allocations, 'service_plan_charge'),
+          //   _.sumBy(event.allocations, 'unknown_category'),
+          //   _.sumBy(event.allocations, 'voice_category'),
+          // ]);
+
+          this.$set(this, 'trendchartData', event.allocations);
+        }
+      });
+
+      this.$http.get(process.env.URL_API + '/users/'+ localStorage.userId +'?include=companies,companies.contents,companies.currentBillMonths,allocations&filter[allocations.billMonth]=[currentBillMonths.last:1]', {
+      }).then((response) => {
+        // console.log('response', response.data);
+        var event = store.sync(response.data);
+        // console.log('piechartData', event.allocations);
+        if (event.allocations && event.allocations.length > 0) {
+          this.$set(this, 'piechartData', event.allocations);
+        }
+      });
     },
     methods:{
       logout() {
@@ -351,8 +370,9 @@
         data: {},
         version : null,
         user: auth.user,
+        // allocations: [],
         piechartData: [],
-        popOver: true
+        trendchartData: [],
       }
     }
   }
