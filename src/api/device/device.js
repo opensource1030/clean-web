46d75dc64ef3590718e4e7f4fa2ba436b09f7bdb
opensource1030 <opensource1030@gmail.com>
2017-01-19 15:26:50 +0100

    CP-1468

--- src/api/device/device.js
+++ src/api/device/device.js
@@ -1,53 +1,39 @@
-
 import Vue from 'vue';
 import auth from './../auth.js';
 import Device from './../../models/Device'
-import {
-  filterByFilters
-} from './../../components/filters.js';
-var {
-  Store,
-} = require('yayson')();
+import {filterByFilters} from './../../components/filters.js';
+var {Store} = require('yayson')();
 var store = new Store();
 
 export default {
   /*-------------------------------update device-----------------*/
-  carriersCheck: {
-    data: [],
+  carriersCheck : {
+    data: []
   },
-  companiesCheck: {
-    data: [],
+  companiesCheck : {
+    data: []
   },
-  modificationsCheck: {
-    data: [],
+  modificationsCheck : {
+    data: []
   },
-
 
   updateDevice(id, context, price, style, capacity, device, image) {
 
-    let deviceObj = new Device('devices',id,device.defaultPrice, device.name, device.description, device.type, 1, image.id,device.make,device.model,device.money);
+    let deviceObj = new Device('devices', id, device.defaultPrice, device.name, device.description, device.type, 1, image.id, device.make, device.model, device.money);
 
-    let check=  this.checkDevice(deviceObj,style,capacity,price,context,id)
-      if(check.staus==false){
-          context.message="Error in field "+check.field;
-        context.showModal=true;
-      }
-      else{
+    let check = this.checkDevice(deviceObj, style, capacity, price, context, id)
+    if (check.staus == false) {
+      context.message = "Error in field " + check.field;
+      context.showModal = true;
+    } else {
 
-
-    context.$http.patch(process.env.URL_API + '/devices/' + id, {
-
-      data: deviceObj.toJSON()
-
-
-    })
-      .then((response) => {
+      context.$http.patch(process.env.URL_API + '/devices/' + id, {data: deviceObj.toJSON()}).then((response) => {
 
         context.$router.push({name: 'devices'});
 
       }, (response) => {
-          context.message=response;
-        context.showModal=true;
+        context.message = response;
+        context.showModal = true;
       });
     }
 
@@ -57,44 +43,36 @@ export default {
 
     context.$http.get(process.env.URL_API + '/devices/' + id, {
       params: {
-        include: 'modifications,devicevariations,devicevariations.companies,devicevariations.carriers,devicevariations.modifications,devicevariations.images,images',
-      },
-
+        include: 'modifications,devicevariations,devicevariations.companies,devicevariations.carriers,devicevariations.modifications,devicevariations.images,images'
+      }
     }).then((response) => {
 
-        event = store.sync(response.data);
+      event = store.sync(response.data);
 
+      context.image.url = process.env.URL_API + '/images/' + event.images[0].id;
+      context.image.id = event.images[0].id;
 
-        context.image.url=process.env.URL_API+'/images/'+event.images[0].id;
-         context.image.id=event.images[0].id;
+      context.d.name = event.name;
+      context.d.description = event.properties;
+      context.d.currency = event.currency;
+      context.d.make = event.make;
+      context.d.model = event.model;
+      context.d.defaultPrice = event.defaultPrice;
+      context.d.type = event.devicetypes[0].id;
+      context.d.make = event.make;
+      context.d.model = event.model;
+      this.carrierCheck(context, filterByFilters(response.data.included, 'carriers'));
+      context.carriers = this.carriersCheck;
+      context.vCarriers = this.carriersCheck;
+      this.companyCheck(context, filterByFilters(response.data.included, 'companies'));
+      context.companies = this.companiesCheck;
+      this.modificationCheck(context, event.modifications);
+      context.modifications = this.modificationsCheck;
+      context.priceData = event.devicevariations;
 
-       context.d.name = event.name;
-        context.d.description = event.properties;
-        context.d.currency = event.currency;
-        context.d.make=event.make;
-        context.d.model=event.model;
-        context.d.defaultPrice=event.defaultPrice;
-       context.d.type = event.devicetypes[0].id;
-        context.d.make=event.make;
-        context.d.model=event.model;
-          this.carrierCheck(context,  filterByFilters(response.data.included,'carriers'));
-        context.carriers = this.carriersCheck;
-        context.vCarriers=this.carriersCheck;
-        this.companyCheck(context, filterByFilters(response.data.included,'companies'));
-        context.companies = this.companiesCheck;
-        this.modificationCheck(context, event.modifications);
-        context.modifications = this.modificationsCheck;
-        context.priceData = event.devicevariations;
+      context.checkcarrier();
 
-        context.checkcarrier();
-
-
-
-      },
-
-      (response) => {
-
-      });
+    }, (response) => {});
 
   },
 
@@ -111,7 +89,6 @@ export default {
           break;
         }
       }
-
 
     }
   },
@@ -158,90 +135,71 @@ export default {
     context.$http.get(process.env.URL_API + '/devicetypes', {
 
       params: {
-        page: page,
-      },
-
+        page: page
+      }
     }).then((response) => {
 
-        context.deviceType = response.data;
+      context.deviceType = response.data;
 
-      },
-
-      (response) => {
-
-      });
+    }, (response) => {});
 
     context.$http.get(process.env.URL_API + '/modifications', {
 
       params: {
-        page: page,
-      },
-
+        page: page
+      }
     }).then((response) => {
-        for (let modification of response.data.data) {
-          this.modificationsCheck.data.push(modification);
-          modification.check = false;
+      for (let modification of response.data.data) {
+        this.modificationsCheck.data.push(modification);
+        modification.check = false;
 
-        }
+      }
 
-        context.modifications = response.data;
+      context.modifications = response.data;
 
-      },
-
-      (response) => {
-
-      });
+    }, (response) => {});
 
     context.$http.get(process.env.URL_API + '/carriers', {
 
       params: {
         page: page,
         'filter[active]': 1,
-        include: 'images',
-      },
-
+        include: 'images'
+      }
     }).then((response) => {
-        event = store.sync(response.data);
-        console.log(event);
+      event = store.sync(response.data);
+      console.log(event);
 
-        //process.env.URL_API
+      //process.env.URL_API
 
-        for (let carrier of event) {
+      for (let carrier of event) {
 
-          this.carriersCheck.data.push(carrier);
-          carrier.check = false;
+        this.carriersCheck.data.push(carrier);
+        carrier.check = false;
 
-        }
+      }
 
-        context.carriers.data = event;
-      },
-
-      (response) => {
-      });
+      context.carriers.data = event;
+    }, (response) => {});
 
     context.$http.get(process.env.URL_API + '/companies', {
 
       params: {
         page: page,
-        'filter[active]': 1,
-      },
-
+        'filter[active]': 1
+      }
     }).then((response) => {
-        context.pagination = response.data.meta.pagination;
-        for (let company of response.data.data) {
+      context.pagination = response.data.meta.pagination;
+      for (let company of response.data.data) {
 
-          this.companiesCheck.data.push(company);
-          company.check = false;
+        this.companiesCheck.data.push(company);
+        company.check = false;
 
-        }
+      }
 
-        context.companies = response.data;
+      context.companies = response.data;
 
-      },
-
-      (response) => {
-
-      });
+    }, (response) => {});
   },
 
   addModifications(context, obj) {
@@ -251,127 +209,157 @@ export default {
         type: 'modifications',
         attributes: {
           modType: obj.type,
-          value: obj.value,
-        },
-      },
-    })
-      .then((response) => {
-        this.getDevice(context);
+          value: obj.value
+        }
+      }
+    }).then((response) => {
+      this.getDevice(context);
 
-      }, (response) => {
-      });
+    }, (response) => {});
   },
-  checkDevice(device,style,capacity,price,context,id){
-          let check=true;
-        if(device.name=="" || device.name==null){
-                check=false
-                let  error={field:"Name",status:false}
-                  return error;
-        }
-        if(isNaN(device.defaultPrice) || device.defaultPrice=="" || device.defaultPrice==null || device.defaultPrice==0 ){
-
-          check=false;
-          let  error={field:"Price",status:false}
-            return error;
-        }
-
-        if(device.properties=="" || device.properties==null){
-
-              check=false
-              let  error={field:"Description",status:false}
-                return error;
-        }
-        if(device.deviceTypeId==null){
-          check=false;
-          let  error={field:"Device Type",status:false}
-            return error;
-        }
-        if(device.imageId==0 ){
-            context.message="Error in field Image";
-          check=false;
-          let  error={field:"Image",status:false}
-            return error;
-        }
-        if(device.make==null || device.make==""){
-          check=false;
-          let  error={field:"Manufactured",status:false}
-            return error;
-        }
-        if(device.model==null || device.model==""){
-
-          check=false;
-        let  error={field:"Model",status:false}
-          return error;
-        }
-        if(check==true && style.length===0 && capacity.length===0 ){
-
-          let  error={field:"",status:true}
-            return error;
-
-        }
-        if(price.length!=0 && check==true){
-            //  device.pricesUpdateJson(price,device);
-            let c=true;
-                  for (let p of price){
-                if(p.style==null || p.capacity==null || p.carrierId==null || p.companyId==null || (p.priceRetail==0 || isNaN(p.priceRetail)) || (p.price1==0 || isNaN(p.price1)) || (p.price2==0 || isNaN(p.price2)) || (p.priceOwn==0 || isNaN(p.priceOwn))){
-                              c=false;
-                              let  error={field:"Table Prices",status:false}
-                                return error;
-
-                    }
-                  }
-            if(c==true){
-                device.modificationsJson(capacity,style,device);
-                if(id==null){
-                device.pricesJson(price,device);
-              }else{
-                device.pricesUpdateJson(price,device)
-              }
-              let  error={field:"",status:true}
-                return error;
-            }
-        }
-        if(check==true && style.length!=0 && capacity.length==0){
-            device.modificationsJson(capacity,style,device);
-            let  error={field:"",status:true}
-              return error;
-        }
-        if(check==true && style.length==0 && capacity.length!=0){
-            device.modificationsJson(capacity,style,device);
-            let  error={field:"",status:true}
-              return error;
-        }
-        if(check==true && style.length!=0 && capacity.length!=0){
-            device.modificationsJson(capacity,style,device);
-            let  error={field:"",status:true}
-              return error;
-        }
-
-  },
-
-
-  addDevice(context, price, style, capacity,device, image) {
-
-  let deviceObj = new Device('devices',null,device.defaultPrice, device.name, device.description, device.type, 1, image.id,device.make,device.model,device.money);
-
-  let check=  this.checkDevice(deviceObj,style,capacity,price,context,null)
-    if(check.status==false){
-        context.message="Error in field "+check.field;
-      context.showModal=true;
+  checkDevice(device, style, capacity, price, context, id) {
+    let check = true;
+    if (device.name == "" || device.name == null) {
+      check = false
+      let error = {
+        field: "Name",
+        status: false
+      }
+      return error;
     }
-    else{
-    context.$http.post(process.env.URL_API + '/devices', {
+    if (isNaN(device.defaultPrice) || device.defaultPrice == "" || device.defaultPrice == null || device.defaultPrice == 0) {
 
-      data: deviceObj.toJSON()
+      check = false;
+      let error = {
+        field: "Price",
+        status: false
+      }
+      return error;
+    }
 
-    })
-      .then((response) => {
+    if (device.properties == "" || device.properties == null) {
 
-          context.$router.push({name: 'devices'});
+      check = false
+      let error = {
+        field: "Description",
+        status: false
+      }
+      return error;
+    }
+    if (device.deviceTypeId == null) {
+      check = false;
+      let error = {
+        field: "Device Type",
+        status: false
+      }
+      return error;
+    }
+    if (device.imageId == 0) {
+      context.message = "Error in field Image";
+      check = false;
+      let error = {
+        field: "Image",
+        status: false
+      }
+      return error;
+    }
+    if (device.make == null || device.make == "") {
+      check = false;
+      let error = {
+        field: "Manufactured",
+        status: false
+      }
+      return error;
+    }
+    if (device.model == null || device.model == "") {
+
+      check = false;
+      let error = {
+        field: "Model",
+        status: false
+      }
+      return error;
+    }
+    if (check == true && style.length === 0 && capacity.length === 0) {
+
+      let error = {
+        field: "",
+        status: true
+      }
+      return error;
+
+    }
+    if (price.length != 0 && check == true) {
+      //  device.pricesUpdateJson(price,device);
+      let c = true;
+      for (let p of price) {
+        if (p.style == null || p.capacity == null || p.carrierId == null || p.companyId == null || (p.priceRetail == 0 || isNaN(p.priceRetail)) || (p.price1 == 0 || isNaN(p.price1)) || (p.price2 == 0 || isNaN(p.price2)) || (p.priceOwn == 0 || isNaN(p.priceOwn))) {
+          c = false;
+          let error = {
+            field: "Table Prices",
+            status: false
+          }
+          return error;
+
+        }
+      }
+      if (c == true) {
+        device.modificationsJson(capacity, style, device);
+        if (id == null) {
+          device.pricesJson(price, device);
+        } else {
+          device.pricesUpdateJson(price, device)
+        }
+        let error = {
+          field: "",
+          status: true
+        }
+        return error;
+      }
+    }
+    if (check == true && style.length != 0 && capacity.length == 0) {
+      device.modificationsJson(capacity, style, device);
+      let error = {
+        field: "",
+        status: true
+      }
+      return error;
+    }
+    if (check == true && style.length == 0 && capacity.length != 0) {
+      device.modificationsJson(capacity, style, device);
+      let error = {
+        field: "",
+        status: true
+      }
+      return error;
+    }
+    if (check == true && style.length != 0 && capacity.length != 0) {
+      device.modificationsJson(capacity, style, device);
+      let error = {
+        field: "",
+        status: true
+      }
+      return error;
+    }
+
+  },
+
+  addDevice(context, price, style, capacity, device, image) {
+
+    let deviceObj = new Device('devices', null, device.defaultPrice, device.name, device.description, device.type, 1, image.id, device.make, device.model, device.money);
+
+    let check = this.checkDevice(deviceObj, style, capacity, price, context, null)
+    if (check.status == false) {
+      context.message = "Error in field " + check.field;
+      context.showModal = true;
+    } else {
+      context.$http.post(process.env.URL_API + '/devices', {data: deviceObj.toJSON()}).then((response) => {
+
+        context.$router.push({name: 'devices'});
 
       }, (response) => {
-          context.message=response;
-          context.showModal=true;
+        context.message = response;
+        context.showModal = true;
 
       });
     }
@@ -379,59 +367,45 @@ export default {
   },
 
   createImage(context, file) {
-    context.$http.post(process.env.URL_API + '/images', file)
+    context.$http.post(process.env.URL_API + '/images', file).then((response) => {
+      context.image.url = 'http://' + response.data.data.links.self;
+      context.image.id = response.data.data.id;
 
-      .then((response) => {
-        context.image.url = 'http://' + response.data.data.links.self;
-        context.image.id = response.data.data.id;
-
-      }, (response) => {
-      });
+    }, (response) => {});
 
   },
-  createImageVariation(context, file,index) {
-    context.$http.post(process.env.URL_API + '/images', file)
+  createImageVariation(context, file, index) {
+    context.$http.post(process.env.URL_API + '/images', file).then((response) => {
+      if (context.id == null) {
+        context.price[index].imageVariations.url = 'http://' + response.data.data.links.self;
+        context.price[index].imageVariations.id = response.data.data.id;
+      } else {
+        context.pricess[index].imageVariations.url = 'http://' + response.data.data.links.self;
+        context.pricess[index].imageVariations.id = response.data.data.id;
 
-      .then((response) => {
-        if(context.id==null){
-            context.price[index].imageVariations.url = 'http://' + response.data.data.links.self;
-            context.price[index].imageVariations.id = response.data.data.id;
-}
-else{
-            context.pricess[index].imageVariations.url = 'http://' + response.data.data.links.self;
-            context.pricess[index].imageVariations.id = response.data.data.id;
-
-
-}
-      }, (response) => {
-      });
+      }
+    }, (response) => {});
 
   },
-  filterCompanies(context,page,companyName){
+  filterCompanies(context, page, companyName) {
     context.$http.get(process.env.URL_API + '/companies', {
 
       params: {
         page: page,
         'filter[name][like]': companyName
-      },
-
+      }
     }).then((response) => {
-        context.pagination = response.data.meta.pagination;
-        for (let company of response.data.data) {
+      context.pagination = response.data.meta.pagination;
+      for (let company of response.data.data) {
 
-          this.companiesCheck.data.push(company);
-          company.check = false;
+        this.companiesCheck.data.push(company);
+        company.check = false;
 
-        }
+      }
 
-        context.companies = response.data;
+      context.companies = response.data;
 
-      },
-
-      (response) => {
-
-      });
-
+    }, (response) => {});
 
   }
 
