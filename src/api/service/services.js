64cc23aa61621b42151b54ed15de6586ab195786
opensource1030 <opensource1030@gmail.com>
2017-01-20 11:44:53 +0100

    CP-1485 #Solving CodeFactor issues

--- src/api/service/services.js
+++ src/api/service/services.js
@@ -17,20 +17,14 @@ export default {
             }
         }).then((response) =>
             {
+                context.showtable = false;
+                context.loading = false;
+
                 if(response.data.data.length == 0){
-                    context.showtable = false;
-                    context.loading = false;
+                    context.errorNotFound = true;
                 } else {
-                    context.showtable = true;
-                    context.loading = false;
 
                     let event = store.sync(response.data);
-
-                    context.pagination = response.data.meta.pagination;
-                    if (response.data.meta.pagination.total_pages > 1){
-                        context.loadpagination = true;
-                    }
-
                     for (let service of event) {
                         service = Object.assign(
                             {},
@@ -43,31 +37,28 @@ export default {
                         services.push(service);
                     }
 
-                    var ok = true;
                     for (let serv of services) {
-                        if(serv.status == null || serv.status == '') { serv.status = ""; }
-                        if(serv.title == null || serv.title == '') { serv.title = ""; }
-                        if(serv.description == null || serv.description == '') { serv.description = ""; }
-                        if(serv.cost == null || serv.cost == '') { serv.cost = ""; }
-                        if(serv.currency == null || serv.currency == '') { serv.currency = "USD"; }
-
                         if(serv.carriers ==  null || serv.carriers.length == 0) {
-                            serv.carriers.push({ presentation : ""});
-                            context.error = "No Carrier Found. Please, Contact with the Administrator.";
-                            ok = false;
+                            context.errorNotFound = true;
                         }
 
                         if (serv.serviceitems == null || serv.serviceitems.length == 0){
-                            context.error = "No ServiceItem Found. Please, Contact with the Administrator.";
-                            ok = false;
+                            context.errorNotFound = true;
                         }
                     }
 
-                    if(ok){
+                    if (!context.errorNotFound) {
+
+                        context.showtable = true;
+                        context.loading = false;
+
+                        context.pagination = response.data.meta.pagination;
+                        if (response.data.meta.pagination.total_pages > 1){
+                            context.loadpagination = true;
+                        }
+
                         context.services = services;
                         context.showtable = true;
-                    } else {
-                        context.showModal = true;
                     }
                 }
             }, (response) => {
