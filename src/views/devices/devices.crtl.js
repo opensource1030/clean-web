a37d4a789d5f189d2fc852bcf3b76174abe0694d
opensource1030 <opensource1030@gmail.com>
2017-02-14 17:36:36 +0100

    issue conflicts

--- src/views/devices/devices.crtl.js
+++ src/views/devices/devices.crtl.js
@@ -1,22 +1,9 @@
 import Tables from './../../components/tableDevices.vue'
 import modal from './../../components/modal.vue';
 export default {
-<<<<<<< HEAD
   components: {
     Tables,
     modal,
-=======
-
-  components : {
-    pagination: Pagination,
-    modal: modal,
-    Multiselect: Multiselect
-  },
-  beforeCreate() {
-    Filter.getCarriers(this);
-    Filter.getModifications(this);
-    Filter.getDeviceTypes(this);
->>>>>>> 92b5b5d... cp-1659 presets
   },
 
   data(){
