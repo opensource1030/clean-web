ceb0dfd810020c6851dc645b3ec2e8fd7267b75c
opensource1030 <opensource1030@gmail.com>
2017-02-17 16:51:23 +0100

    fix issues with master

--- src/views/packages/Packages.vue
+++ src/views/packages/Packages.vue
@@ -1,12 +1,11 @@
 <template>
-  <div class="content-right">
+
     <div id="tables">
       <div class="header"></div>
         <div class="small-12 columns titles">
           <h4>Manage Packages<h4>
         </div>
       </div>
-    </div>
-  </div>
+
 </template>
 <script  src="./packages.crtl.js" lang="babel"  ></script>
