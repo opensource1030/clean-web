725325ff5cca4e46d05d4780bc87ae88fdb9ce81
opensource1030 <opensource1030@gmail.com>
2017-03-02 14:21:16 -0500

    updates production https URL

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,9 +1,9 @@
 module.exports = {
     NODE_ENV: '"production"',
-    URL_API: '"http://api.wirelessanalytics.com"',
-    URL: '"http://beta.wirelessanalytics.com"',
+    URL_API: '"https://api.wirelessanalytics.com"',
+    URL: '"https://beta.wirelessanalytics.com"',
     CLIENT_ID: '"2"',
     CLIENT_SECRET: '"ab9QdKGBXZmZn50aPlf4bLlJtC4BJJNC0M99i7B7"',
-    LEGACY_URL: '"http://app.wirelessanalytics.com"',
+    LEGACY_URL: '"https://app.wirelessanalytics.com"',
 };
 
