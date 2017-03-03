251c4de7b670888deca9dafc4066ed4e9c4a005e
opensource1030 <opensource1030@gmail.com>
2017-03-03 15:07:46 +0100

    test list devices

--- test/e2e/custom-commands/routes.js
+++ test/e2e/custom-commands/routes.js
@@ -1,11 +1,15 @@
 exports.command = function(section, redirect, callback) {
-    var self = this;
     this
         .frame(null)
         .waitForElementVisible('a[name="'+section+'"]', 15000)
         .click('a[name="'+section+'"]')
-        .waitForElementVisible('a[name="'+redirect+'"]', 15000)
+        .waitForElementVisible('a[name="'+redirect+'"]', 25000)
         .click('a[name="'+redirect+'"]')
-
+        .windowHandles(function(result) {
+            let newWindow;
+              this.verify.equal(result.value.length, 2, 'There should be 2 windows open');
+                newWindow = result.value[1];
+                  this.switchWindow(newWindow);
+})
     return this;
 };
--- test/e2e/specs/devices.js
+++ test/e2e/specs/devices.js
@@ -6,22 +6,35 @@ module.exports = {
     const devServer = browser.globals.devServerURL;
   browser
     .url(devServer)
-    .login('rgonzalez@zipcar.com', 'zpncdlliptncojmyaqko')
+    .login('Sample3433@email.com', 'user')
     .routes('Inventory','Devices')
-      .waitForElementVisible('#app  #tables', 15000)
-      .assert.urlEquals(devServer+'/devices')
+
+
 
 },
 ' DevicesList': function (browser) {
 const devServer = browser.globals.devServerURL;
     browser
+      .assert.urlEquals(devServer+'/devices')
+        .waitForElementVisible('#app  #tables', 15000)
+      .waitForElementVisible('table', 5000)
+      .waitForElementVisible('tbody', 5000)
+      .waitForElementVisible('#open', 5000)
+      .click('#open')
+        .waitForElementVisible('.detail', 5000)
+        .waitForElementVisible('.information span', 5000)
+        .assert.containsText(".information span","Technical Information" )
     .pause(1000)
 
+},
+'AddDevice': function (browser) {
+  const devServer = browser.globals.devServerURL;
+      browser
+            .click('.buttonTable')
+        .assert.urlEquals(devServer+'/device')
 
-
-
-
-
+},
+'ManageDevice': function (browser) {
 
 
 }
