8b81a2f19833c072c9fd7a96c9465fda43691cf7
opensource1030 <opensource1030@gmail.com>
2017-03-09 12:18:02 +0100

    cp-1725 Finish services List

--- src/store/modules/services/getters.js
+++
@@ -1,3 +0,0 @@
-export const getServices = state => state.all
-export const getSelect = state =>  state.selects
-export const getValues= state=> state.values
--- src/store/modules/services/mutation-types.js
+++
@@ -1,2 +0,0 @@
-export const SERVICE_GET_ALL = 'services/GET_ALL'
-export const SERVICE_ADD_NEW = 'services/ADD_NEW'
