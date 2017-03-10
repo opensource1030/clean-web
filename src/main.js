fff51d5ba90f7eda038dc938b5ab0cff959d6f50
opensource1030 <opensource1030@gmail.com>
2017-03-10 17:11:05 -0500

    fixes the wrong vue charts reference

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import 'script!select2'
 
 import 'script!jquery.cookie'
 
-import VueCharts from './../node_modules/vue-charts/dist/vue-charts.js'
+import VueCharts from './../node_modules/vue-chartjs/dist/vue-chartjs.js'
 import './../node_modules/slicknav/dist/jquery.slicknav.js'
 
 import './../node_modules/stacktable.js/stacktable.js'
