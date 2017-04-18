d4ce371db4003e14706e50d8a3bfdab1706143b1
opensource1030 <opensource1030@gmail.com>
2017-04-19 00:09:50 +0545

    CP-1748 menu links redirect to legacy with alert
    Signed-off-by: Sushil Parajuli <sushilparajuli2010@gmail.com>

--- src/components/Sidemenu.vue
+++ src/components/Sidemenu.vue
@@ -1,74 +1,60 @@
 <template>
-    <section class="menu-left test">
+    <section class="menu-left ">
         <ul id="menu" class="sidebar-menu">
             <li class="menu-toggle">
                 <a class="icon-close"><i class="fa fa-bars"></i></a>
             </li>
             <li class="menu-title">
                 <a href="/dashboard">
-                    <i class="fa fa-home"></i> <span>Home</span>
+                    <i class="fa fa-dashboard"></i> <span>Dashboard</span>
                 </a>
             </li>
 
-            <template v-if="features.FEATURE_IN_DEVELOPMENT">
-                <li v-permission="'Procurements'"  class="treeview">
-                    <a href="javascript:;"  name="Inventory" >
-                        <i class="fa fa-list-alt"></i>
-                        <span>Inventory</span>
-                        <i class="fa fa-plus pull-right"></i>
-                        <i class="fa fa-minus pull-right"></i>
-                    </a>
-                    <ul class="treeview-menu">
-                        <li class="page-link" v-permission="'ManageDevices'">
-                            <a class="admin" name="Devices" href="/devices"><i class="fa fa-circle-o"></i>Equipment</a>
-                        </li>
-                        <li class="page-link" v-permission="'ManageServices'">
-                            <a class="admin" name="services" href="/services"><i class="fa fa-circle-o"></i>Services & Plans</a>
-                        </li>
-                    </ul>
-                </li>
+            <li class="treeview">
+                <a href="#">
+                    <i class="fa fa-bar-chart"></i>
+                    <span>Reports</span>
+                    <i class="fa fa-plus pull-right"></i>
+                    <i class="fa fa-minus pull-right"></i>
+                </a>
+                <ul class="treeview-menu">
+                    <li class="redirect-link"><a target="_blank"
+                                                 href="http://app.wirelessanalytics.com/helpdesk/udl/dashboard_trend.asp?access_token="><i
+                            class="fa fa-circle-o"></i> Trends</a></li>
+                    <li class="redirect-link"><a target="_blank"
+                                                 href="http://app.wirelessanalytics.com/helpdesk/udl/dashboard_cycle.asp?access_token="><i
+                            class="fa fa-circle-o"></i> Bill Cycle</a></li>
+                    <li class="redirect-link"><a target="_blank"
+                                                 href="http://app.wirelessanalytics.com/helpdesk/udl/dashboard_top_ten.asp?access_token="><i
+                            class="fa fa-circle-o"></i> Top 10 Reports</a></li>
+                    <li class="redirect-link"><a target="_blank"
+                                                 href="http://app.wirelessanalytics.com/helpdesk/udl/report_allocation.asp?access_token="><i
+                            class="fa fa-circle-o"></i> Charge</a></li>
+                    <li class="redirect-link"><a target="_blank"
+                                                 href="http://app.wirelessanalytics.com/helpdesk/udl/report_data.asp?access_token="><i
+                            class="fa fa-circle-o"></i> Data</a></li>
+                    <li class="redirect-link"><a target="_blank"
+                                                 href="http://app.wirelessanalytics.com/helpdesk/udl/report_international.asp?access_token="><i
+                            class="fa fa-circle-o"></i> International</a></li>
+                    <li class="redirect-link"><a target="_blank"
+                                                 href="http://app.wirelessanalytics.com/helpdesk/udl/report_usage.asp?access_token="><i
+                            class="fa fa-circle-o"></i> Usage</a></li>
+                    <li class="redirect-link"><a target="_blank"
+                                                 href="http://app.wirelessanalytics.com/helpdesk/udl/report_ap.asp?access_token="><i
+                            class="fa fa-circle-o"></i> Intercompany Charge</a></li>
+                    <li class="redirect-link"><a target="_blank"
+                                                 href="http://app.wirelessanalytics.com/helpdesk/udl/report_zero_usage.asp?access_token="><i
+                            class="fa fa-circle-o"></i> Zero Usage</a></li>
+                </ul>
+            </li>
+            <li>
+                <router-link :to="{ name: 'legacyInfo'}">
+                    <i class="fa fa-phone"></i> <span>Support</span>
+                </router-link>
 
-                <li v-permission="'Packages'" class="treeview">
-                    <a href="javascript:;" name="Polices">
-                        <i class="fa fa-th-large"></i>
-                        <span>Policies</span>
-                        <i class="fa fa-plus pull-right"></i>
-                        <i class="fa fa-minus pull-right"></i>
-                    </a>
-                    <ul class="treeview-menu">
-                        <li><a name="AllPackages" href="/packages"><i class="fa fa-circle-o"></i>View All Packages</a></li>
-                        <li><a name="createPackage" href="/package"><i class="fa fa-circle-o"></i>Create a Package</a></li>
-                    </ul>
-                </li>
 
-                <li v-permission="'Presets'" class="treeview">
-                    <a href="javascript:;" name="presets">
-                        <i class="fa fa-tasks"></i>
-                        <span>Presets</span>
-                        <i class="fa fa-plus pull-right"></i>
-                        <i class="fa fa-minus pull-right"></i>
-                    </a>
-                    <ul class="treeview-menu">
-                        <li><a name="device" href="/presets"><i class="fa fa-circle-o"></i>Device</a></li>
-                        <li><a name="app" href="javascript:;"><i class="fa fa-circle-o"></i>App</a></li>
-                        <li><a name="address"  href="javascript:;"><i class="fa fa-circle-o"></i>Addresses</a></li>
-                    </ul>
-                </li>
+            </li>
 
-                <li v-permission="'Presets'" class="treeview">
-                    <a href="javascript:;" name="configuration">
-                        <i class="fa fa-tasks"></i>
-                        <span>Configuration</span>
-                        <i class="fa fa-plus pull-right"></i>
-                        <i class="fa fa-minus pull-right"></i>
-                    </a>
-                    <ul class="treeview-menu">
-                        <li><a name="portal" href="javascript:;"><i class="fa fa-circle-o"></i>Portal</a></li>
-                        <li><a name="procurement" href="javascript:;"><i class="fa fa-circle-o"></i>Procurement</a></li>
-                        <li><a name="companies" href="/companies"><i class="fa fa-circle-o"></i>Companies</a></li>
-                    </ul>
-                </li>
-            </template>
         </ul>
 
         <div class="nav-poweredby">
@@ -106,11 +92,11 @@
                 var id = localStorage.userId;
                 var email = localStorage.email;
                 $('.redirect-link a').attr('href', function (index, href) {
-                    var param = 'access_token=' + token + '&email=' + email;
+                    var param = token + '&email=' + email;
                     if (href.charAt(href.length - 1) === '?') //Very unlikely
                         return href + param;
                     else if (href.indexOf('?') > 0)
-                        return href + '&' + param;
+                        return href + param;
                     else
                         return href + '?' + param;
                 });
@@ -157,24 +143,25 @@
             $.sidebarMenu($('.sidebar-menu'));
            /* $.cookie("isMenuActive", "1");*/
             if ($.cookie("isMenuActive") == 1) {
-                $('.menu-left').toggleClass("test");
-                $('.content-right').toggleClass("test");
+                $('.menu-left').addClass("test");
+                $('.content-right').addClass("test");
             }
             $(".icon-close").click(function () {
                 if ($(".menu-left").hasClass("test") == true) {
                     //button was active, de-activate it and update cookie
                     $(".menu-left").removeClass("test");
                     $(".content-right").removeClass("test");
-                    $.cookie("isMenuActive", "1");
+                    $.cookie("isMenuActive", "0");
                 }
                 else {
                     //button is not active. add active class and update cookie.
                     $(".menu-left").addClass("test");
                      $(".content-right").addClass("test");
-                    $.cookie("isMenuActive", "0");
+                    $.cookie("isMenuActive", "1");
                 }
             });
             $('#menu').slicknav({prependTo: 'section.top-bar-section'});
         },
     }
 </script>
+
