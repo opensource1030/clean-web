b52305d4c020f6cb4281bf68a25eb8bc45eb5c1e
opensource1030 <opensource1030@gmail.com>
2017-07-21 23:06:21 +0800

    check some required fields in mapping; email, firstName, lastName, supervisorEmail

--- src/views/employees/employee.bulk.mapping.ctrl.js
+++ src/views/employees/employee.bulk.mapping.ctrl.js
@@ -39,12 +39,13 @@ export default {
    */
   created () {
     let companyuserimportjobs = this.$store.state.employee_bulk.companyuserimportjobs
+    // let res = {"data":{"type":"companyuserimportjobs","id":"11","attributes":{"jobType":"employeeimportjob","companyId":1,"path":"\/home\/forge\/dev.api.wirelessanalytics.com\/releases\/20170720201921\/storage\/clients\/jenkins-mitchell-and-hackett\/employee-1500635837.csv","file":"employee-1500635837.csv","totalUsers":0,"createdUsers":0,"creatableUsers":0,"updatedUsers":0,"updatableUsers":2,"failedUsers":0,"CSVfields":["email","alternateEmail","password","username","confirmation_code","confirmed","firstName","lastName","alternateFirstName","supervisorEmail","companyUserIdentifier","isSupervisor","isValidator","isActive","defaultLang","level","notify","companyId","supervisorId","externalId","approverId","defaultLocationId"],"DBfields":{"email":"Main Email","alternateEmail":"Alternate Email","username":"Username","firstName":"First Name","lastName":"Last Name","supervisorEmail":"Supervisor Email","companyUserIdentifier":"Company User Identifier","isSupervisor":"Is Supervisor?","isValidator":"Is Validator?","isActive":"Is Active?","hierarchy":"Hierarchy","defaultLang":"Default Language","notes":"Notes","level":"Level","Cost Center":"Cost Center","Division":"Division","Position":"Position"},"sampleUser":{"email":"douglas.rolfson@example.org","alternateEmail":"pagac.ashlee@example.org","password":"user","username":"douglas.rolfson","confirmation_code":"b95c05f09018e7d91c5a67c8d66b68f4","confirmed":"1","firstName":"Britney","lastName":"Prosacco","alternateFirstName":"Larissa","supervisorEmail":"leon62@example.org","companyUserIdentifier":"2","isSupervisor":"0","isValidator":"0","isActive":"1","defaultLang":"en","level":"0","notify":"0","companyId":"3","supervisorId":"3","externalId":"\\N","approverId":"1","defaultLocationId":"51"},"mappings":{},"status":"Pending","created_by_id":1,"updated_by_id":1,"created_at":{"date":"2017-07-21 11:17:17.000000","timezone_type":3,"timezone":"UTC"},"updated_at":{"date":"2017-07-21 11:17:17.000000","timezone_type":3,"timezone":"UTC"}},"links":{"self":"dev.api.wirelessanalytics.com\/companyuserimportjobs\/11"}}}
     // let res = {"data":{"type":"companyuserimportjobs","id":"11","attributes":{"jobType":"employeeimportjob","companyId":1,"path":"\/home\/forge\/dev.api.wirelessanalytics.com\/releases\/20170720165457\/storage\/clients\/ullrich-ltd\/employee-1500577003.csv","file":"employee-1500577003.csv","totalUsers":0,"createdUsers":0,"creatableUsers":0,"updatedUsers":0,"updatableUsers":2,"failedUsers":0,"CSVfields":["email","alternateEmail","password","username","confirmation_code","confirmed","firstName","lastName","alternateFirstName","supervisorEmail","companyUserIdentifier","isSupervisor","isValidator","isActive","defaultLang","level","notify","companyId","supervisorId","externalId","approverId","defaultLocationId"],"DBfields":{"email":"Main Email","alternateEmail":"Alternate Email","username":"Username","firstName":"First Name","lastName":"Last Name","supervisorEmail":"Supervisor Email","companyUserIdentifier":"Company User Identifier","isSupervisor":"Is Supervisor?","isValidator":"Is Validator?","isActive":"Is Active?","hierarchy":"Hierarchy","defaultLang":"Default Language","notes":"Notes","level":"Level","Cost Center":"Cost Center","Division":"Division","Position":"Position"},"sampleUser":{"email":"douglas.rolfson@example.org","alternateEmail":"pagac.ashlee@example.org","password":"user","username":"douglas.rolfson","confirmation_code":"b95c05f09018e7d91c5a67c8d66b68f4","confirmed":"1","firstName":"Britney","lastName":"Prosacco","alternateFirstName":"Larissa","supervisorEmail":"leon62@example.org","companyUserIdentifier":"2","isSupervisor":"0","isValidator":"0","isActive":"1","defaultLang":"en","level":"0","notify":"0","companyId":"3","supervisorId":"3","externalId":"\\N","approverId":"1","defaultLocationId":"51"},"mappings":[{"dbField":"email","csvField":"email"},{"dbField":"alternateEmail","csvField":"alternateEmail"},{"dbField":"username","csvField":"username"},{"dbField":"firstName","csvField":"firstName"},{"dbField":"lastName","csvField":"lastName"},{"dbField":"supervisorEmail","csvField":"supervisorEmail"},{"dbField":"companyUserIdentifier","csvField":"companyUserIdentifier"},{"dbField":"isSupervisor","csvField":"isSupervisor"},{"dbField":"isValidator","csvField":"isValidator"},{"dbField":"isActive","csvField":"isActive"},{"dbField":"defaultLang","csvField":"defaultLang"},{"dbField":"level","csvField":"level"}],"status":"Pending","created_by_id":1,"updated_by_id":1,"created_at":{"date":"2017-07-20 18:56:43.000000","timezone_type":3,"timezone":"UTC"},"updated_at":{"date":"2017-07-20 18:56:43.000000","timezone_type":3,"timezone":"UTC"}},"links":{"self":"dev.api.wirelessanalytics.com\/companyuserimportjobs\/11"}}}
     // let companyuserimportjobs = res.data.attributes
-    console.log('companyuserimportjobs', companyuserimportjobs)
+    // console.log('companyuserimportjobs', companyuserimportjobs)
 
     let mappings = companyuserimportjobs.mappings
-    let dbfields
+    let dbfields = companyuserimportjobs.DBfields
 
     // if (_.isEmpty(companyuserimportjobs.DBfields)) {
     //   dbfields = {
@@ -81,7 +82,7 @@ export default {
 
     this.sample_user = companyuserimportjobs.sampleUser;
 
-    // console.log(_.isEmpty(mappings), mappings, this.db_fields, this.csv_fields)
+    console.log(_.isEmpty(mappings), mappings, this.db_fields, this.csv_fields)
     if (_.isEmpty(mappings)) {
       for (let idx1 = 0; idx1 < this.db_fields.length; idx1 ++) {
         let match_flag = false
@@ -119,6 +120,19 @@ export default {
   methods: {
     submit () {
       var validation_flag = true;
+      if (! (_.find(this.db_matched_fields, (field) => { return field.name == 'email'}) &&
+          _.find(this.db_matched_fields, (field) => { return field.name == 'firstName'}) &&
+          _.find(this.db_matched_fields, (field) => { return field.name == 'lastName'}) &&
+          _.find(this.db_matched_fields, (field) => { return field.name == 'supervisorEmail'}))) {
+        validation_flag = false;
+      }
+
+      if (!validation_flag) {
+        console.log('missing required db field');
+        this.$store.dispatch('error/addNew', { message: 'Please map [Main Email], [Firs tName], [Last Name], [Supervisor Email]' })
+        return
+      }
+
       for (var idx = 0; idx < this.db_matched_fields.length; idx++) {
         if (this.db_matched_fields[idx].name != 'Unkown ignore') {
           if (this.csv_matched_fields[idx] == 'Unkown ignore') {
@@ -131,6 +145,7 @@ export default {
           this.mappings.push(dict)
         }
       }
+
       if (validation_flag) {
         this.$store.dispatch('employee_bulk/updateMappings', this.mappings).then(
           res => {
