be1741565fd87863debe03553a914eaf96b0bcce
opensource1030 <opensource1030@gmail.com>
2018-11-30 22:42:17 -0500

    updates prod info

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,10 +1,10 @@
 module.exports = {
     NODE_ENV: '"production"',
     URL_API: '"https://api.wirelessanalytics.com"',
-    URL: '"https://beta.wirelessanalytics.com"',
+    URL: '"https://app.wirelessanalytics.com"',
     CLIENT_ID: '"2"',
     CLIENT_SECRET: '"ab9QdKGBXZmZn50aPlf4bLlJtC4BJJNC0M99i7B7"',
-    LEGACY_URL: '"https://app.wirelessanalytics.com"',
+    LEGACY_URL: '"https://legacy.wirelessanalytics.com"',
     SUPPORT_EMAIL: '"easyvista@wirelessanalytics.com"',
     EASYVISTA_CODE: 16,
     EV_ACCOUNT: 50004,
