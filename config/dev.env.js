d34636578e02a617848e532bb818b476857a0977
opensource1030 <opensource1030@gmail.com>
2019-09-10 16:22:43 -0400

    bump release

--- config/dev.env.js
+++ config/dev.env.js
@@ -10,7 +10,7 @@ module.exports = {
   EASYVISTA_CODE: 13,
   EZW_SSO_URL: 'https://oauth2.eztest.nu/sso?partner=WIRELESS',
   AIRSHIP_KEY: 'm28fzq7j0yrpr8us',
-  VERSION:'v2019.8.19',
+  VERSION:'v2019.8.20',
   EV_URL: 'https://wa.easyvista.com/api/v1',
   BUGSNAG_API_KEY: 'ff1b86eaaf4a5afb137f0a1d046256bb'
 }
