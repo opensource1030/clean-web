e8aaba96a5cc453ad733410b327ed5503f71534e
opensource1030 <opensource1030@gmail.com>
2019-09-12 16:17:08 -0500

    595 - service form validation partial

--- src/views/services/edit.ctrl.js
+++ src/views/services/edit.ctrl.js
@@ -1,6 +1,7 @@
 import {mapGetters, mapActions} from 'vuex'
 import { Switch as cSwitch } from '@coreui/vue'
 import { ServiceHelper } from '@/helpers'
+import _ from 'lodash'
 
 export default {
 
@@ -19,9 +20,35 @@ export default {
       planCarrier: null,
       checker: null,
       isLoading: true,
-      validators: {
-        planCodeError: null
-      }
+      //------
+      formValidators: {
+        generalErr                  : false,
+        maxNum                      : 999999,
+        minNum                      : 0,
+        addonCounter                : 0,
+        flags: {
+          costErr                   : null,
+          domesticMinutesErr        : null,
+          domesticDataErr           : null,
+          domesticSMSErr            : null,
+          internationalMinutesErr   : null,
+          internationalDataErr      : null,
+          internationalSMSErr       : null,
+        },
+        addonFlags: {
+          addon0Cost                : null,
+        },
+      },
+      //------
+      unlimitedSwitches: {
+        domesticMinutes             : false,
+        domesticData                : false,
+        domesticSMS                 : false,
+        internationalMinutes        : false,
+        internationalData           : false,
+        internationalSMS            : false,
+      },
+      //------
     }
   },
 
@@ -33,14 +60,6 @@ export default {
       addons: 'service/getAddons',
       carriers: 'carrier/sorted'
     }),
-    // validators() {
-    //   return {
-    //     planCode: {
-    //       value: this.$store.state.service.serviceDetails.code,
-    //       error: null,
-    //     }
-    //   }
-    // }
   },
 
   created() {
@@ -61,12 +80,88 @@ export default {
   methods: {
     save() {
 
-      if (parseInt(this.serviceDetails.code) > 999999) {
-        this.validators.planCodeError = false
+      let self = this
+
+      // Validation begin
+
+      let maxNum      = self.formValidators.maxNum
+      let minNum      = self.formValidators.minNum
+      let flags       = self.formValidators.flags
+      let addonFlags  = self.formValidators.addonFlags
+
+      if ( parseInt(self.serviceDetails.cost) > maxNum || parseInt(self.serviceDetails.cost) < minNum) {
+        flags.costErr = false
+      } else {
+        flags.costErr = null
+      }
+
+      if ( parseInt(self.domesticPlan.minutes.value) > maxNum || parseInt(self.domesticPlan.minutes.value) < minNum) {
+        flags.domesticMinutesErr = false
+      } else {
+        flags.domesticMinutesErr = null
+      }
+
+      if ( parseInt(self.domesticPlan.data.value) > maxNum || parseInt(self.domesticPlan.data.value) < minNum) {
+        flags.domesticDataErr = false
+      } else {
+        flags.domesticDataErr = null
+      }
+
+      if ( parseInt(self.domesticPlan.sms.value) > maxNum || parseInt(self.domesticPlan.sms.value) < minNum) {
+        flags.domesticSMSErr = false
+      } else {
+        flags.domesticSMSErr = null
+      }
+
+      if ( parseInt(self.internationalPlan.minutes.value) > maxNum || parseInt(self.internationalPlan.minutes.value) < minNum) {
+        flags.internationalMinutesErr = false
+      } else {
+        flags.internationalMinutesErr = null
+      }
+
+      if ( parseInt(self.internationalPlan.data.value) > maxNum || parseInt(self.internationalPlan.data.value) < minNum) {
+        flags.internationalDataErr = false
+      } else {
+        flags.internationalDataErr = null
+      }
+
+      if ( parseInt(self.internationalPlan.sms.value) > maxNum || parseInt(self.internationalPlan.sms.value) < minNum) {
+        flags.internationalSMSErr = false
+      } else {
+        flags.internationalSMSErr = null
+      }
+
+      for (let i = 0; i < self.addons.length; i++) {
+        if ( parseInt(self.addons[i].cost) > maxNum || parseInt(self.addons[i].cost) < minNum) {
+          self.formValidators.addonFlags['addon' + i.toString() + 'Cost'] = false
+        } else {
+          self.formValidators.addonFlags['addon' + i.toString() + 'Cost'] = null
+        }
+      }
+
+      self.formValidators.generalErr = false
+
+      Object.values(flags).forEach(val => {
+        if ( val === false ) {
+          self.formValidators.generalErr = true
+        }
+      })
+
+      // Now check for the addons flags
+
+      Object.values(addonFlags).forEach(val => {
+        if ( val === false ) {
+          self.formValidators.generalErr = true
+        }
+      })
+
+      if (self.formValidators.generalErr) {
         return
       }
 
-      let self = this
+      // Validation end
+
+      //let self = this
       let valid = ServiceHelper.validateFields(self.serviceDetails, self.addons)
 
       self.$store.dispatch('error/clearAll')
@@ -77,5 +172,30 @@ export default {
 
       self.$store.dispatch('service/save', { router: self.$router })
     },
+
+    addFormAddonValidator() {
+      let self = this
+      let addonFlags = self.formValidators.addonFlags
+
+      // reset addons validator flag object
+      self.formValidators.addonFlags = _.mapValues(addonFlags, () => null)
+
+      self.formValidators.addonCounter++
+      self.formValidators.addonFlags['addon' + self.formValidators.addonCounter.toString() + 'Cost'] = null
+    },
+
+    removeFormAddonValidator() {
+      let self = this
+      self.formValidators.addonCounter--
+
+      // Re-do the addons validator flag object
+      self.formValidators.addonFlags = {}
+
+      for (let i = 0; i < self.addons.length; i++) {
+        self.formValidators.addonFlags['addon' + self.formValidators.addonCounter.toString() + 'Cost'] = null
+      }
+
+    },
+
   }
 }
--- src/views/services/edit.vue
+++ src/views/services/edit.vue
@@ -29,14 +29,19 @@
                 <div class="row">
                   <div class="col-sm-5">
                     <label>Plan Code</label>
-                    <b-form-input v-model="serviceDetails.code" :state="validators.planCodeError" type="text" class="form-control" :value="serviceDetails.code"
-                                  @input="$store.commit('service/updateServiceDetail',{e:$event,type:'code'})" />
-                    <p v-if="validators.planCodeError === false" class="mb-0 mt-2 text-danger">Number must be between 0 and 999.999</p>
+                    <b-form-input type="text" class="form-control"
+                      :value="serviceDetails.code"
+                      @input="$store.commit('service/updateServiceDetail',{e:$event,type:'code'})"
+                    />
                   </div>
                   <div class="col-sm-4">
                     <label>Cost</label>
-                    <b-form-input type="text" class="form-control" :value="serviceDetails.cost"
-                                  @input="$store.commit('service/updateServiceDetail',{e:$event,type:'cost'})" />
+                    <b-form-input type="text" class="form-control"
+                      :state="formValidators.flags.costErr"
+                      :value="serviceDetails.cost"
+                      @input="$store.commit('service/updateServiceDetail',{e:$event,type:'cost'})"
+                    />
+                    <p v-if="formValidators.flags.costErr === false" class="mb-0 mt-2 text-danger">Must be between 0 and 999.999</p>
                   </div>
                   <div class="col-sm-3">
                     <label>Currency</label>
@@ -99,9 +104,22 @@
                   <b-input-group-prepend>
                     <b-input-group-text>Amount</b-input-group-text>
                   </b-input-group-prepend>
-                  <b-form-input type="number" class="form-control" :value="domesticPlan.minutes.value"
-                                @input="$store.commit('service/updateDomesticplan',{e:$event,type:'minutes'})" />
+                  <b-form-input type="number" class="form-control"
+                    :value="domesticPlan.minutes.value"
+                    @input="$store.commit('service/updateDomesticplan',{e:$event,type:'minutes'})"
+                    :state="formValidators.flags.domesticMinutesErr"
+                    :disabled="!unlimitedSwitches.domesticMinutes"
+                    />
+                    <p v-if="formValidators.flags.domesticMinutesErr === false" class="mb-0 mt-2 text-danger">Must be between 0 and 999.999</p>
                 </b-input-group>
+                <div class="d-flex">
+                  <label class="block">Unlimited Minutes</label>
+                  <c-switch
+                    color="primary"
+                    :checked="unlimitedSwitches.domesticMinutes"
+                    @change="unlimitedSwitches.domesticMinutes = !unlimitedSwitches.domesticMinutes"
+                  />
+                </div>
               </div>
               <div class="col-sm-4 col-xs-12">
                 <label><i class="fa fa-database"></i> Data</label>
@@ -109,8 +127,12 @@
                   <b-input-group-prepend>
                     <b-input-group-text>Amount</b-input-group-text>
                   </b-input-group-prepend>
-                  <b-form-input type="number" class="form-control" :value="domesticPlan.data.value"
-                                @input="$store.commit('service/updateDomesticplan',{e:$event,type:'data'})" />
+                  <b-form-input type="number" class="form-control"
+                    :value="domesticPlan.data.value"
+                    @input="$store.commit('service/updateDomesticplan',{e:$event,type:'data'})"
+                    :state="formValidators.flags.domesticDataErr"
+                    :disabled="!unlimitedSwitches.domesticData"
+                    />
                   <multiselect
                     placeholder="Unit"
                     :options="dataUnit"
@@ -118,7 +140,16 @@
                     :show-labels="false"
                     :value="domesticPlan.data.unit"
                     @input="$store.commit('service/updateDomesticplan',{e:$event,type:'unit'})" />
+                  <p v-if="formValidators.flags.domesticDataErr === false" class="mb-0 mt-2 text-danger">Must be between 0 and 999.999</p>
                 </b-input-group>
+                <div class="d-flex">
+                  <label class="block">Unlimited Data</label>
+                  <c-switch
+                    color="primary"
+                    :checked="unlimitedSwitches.domesticData"
+                    @change="unlimitedSwitches.domesticData = !unlimitedSwitches.domesticData"
+                  />
+                </div>
               </div>
               <div class="col-sm-4 col-xs-12">
                 <label><i class="fa fa-commenting"></i> SMS</label>
@@ -126,9 +157,22 @@
                   <b-input-group-prepend>
                     <b-input-group-text>Amount</b-input-group-text>
                   </b-input-group-prepend>
-                  <b-form-input type="number" class="form-control" :value="domesticPlan.sms.value"
-                                @input="$store.commit('service/updateDomesticplan',{e:$event,type:'sms'})" />
+                  <b-form-input type="number" class="form-control"
+                    :value="domesticPlan.sms.value"
+                    @input="$store.commit('service/updateDomesticplan',{e:$event,type:'sms'})"
+                    :disabled="!unlimitedSwitches.domesticSMS"
+                    :state="formValidators.flags.domesticSMSErr"
+                  />
+                  <p v-if="formValidators.flags.domesticSMSErr === false" class="mb-0 mt-2 text-danger">Must be between 0 and 999.999</p>
                 </b-input-group>
+                <div class="d-flex">
+                  <label class="block">Unlimited SMS</label>
+                  <c-switch
+                    color="primary"
+                    :checked="unlimitedSwitches.domesticSMS"
+                    @change="unlimitedSwitches.domesticSMS = !unlimitedSwitches.domesticSMS"
+                  />
+                </div>
               </div>
             </div>
           </b-card-body>
@@ -148,9 +192,22 @@
                   <b-input-group-prepend>
                     <b-input-group-text>Amount</b-input-group-text>
                   </b-input-group-prepend>
-                  <b-form-input type="number" class="form-control" :value="internationalPlan.minutes.value"
-                                @input="$store.commit('service/updateInternationalplan',{e:$event,type:'minutes'})" />
+                  <b-form-input type="number" class="form-control"
+                    :value="internationalPlan.minutes.value"
+                    @input="$store.commit('service/updateInternationalplan',{e:$event,type:'minutes'})"
+                    :state="formValidators.flags.internationalMinutesErr"
+                    :disabled="!unlimitedSwitches.internationalMinutes"
+                  />
+                  <p v-if="formValidators.flags.internationalMinutesErr === false" class="mb-0 mt-2 text-danger">Must be between 0 and 999.999</p>
                 </b-input-group>
+                <div class="d-flex">
+                  <label class="block">Unlimited Minutes</label>
+                  <c-switch
+                    color="primary"
+                    :checked="unlimitedSwitches.internationalMinutes"
+                    @change="unlimitedSwitches.internationalMinutes = !unlimitedSwitches.internationalMinutes"
+                  />
+                </div>
               </div>
               <div class="col-sm-4 col-xs-12">
                 <label><i class="fa fa-database"></i> Data</label>
@@ -159,7 +216,10 @@
                     <b-input-group-text>Amount</b-input-group-text>
                   </b-input-group-prepend>
                   <b-form-input type="number" class="form-control" :value="internationalPlan.data.value"
-                                @input="$store.commit('service/updateInternationalplan',{e:$event,type:'data'})" />
+                    @input="$store.commit('service/updateInternationalplan',{e:$event,type:'data'})"
+                    :disabled="!unlimitedSwitches.internationalData"
+                    :state="formValidators.flags.internationalDataErr"
+                                />
                   <multiselect
                     placeholder="Unit"
                     :options="dataUnit"
@@ -167,7 +227,16 @@
                     :show-labels="false"
                     :value="internationalPlan.data.unit"
                     @input="$store.commit('service/updateInternationalplan',{e:$event,type:'unit'})" />
+                  <p v-if="formValidators.flags.internationalDataErr === false" class="mb-0 mt-2 text-danger">Must be between 0 and 999.999</p>
                 </b-input-group>
+                <div class="d-flex">
+                  <label class="block">Unlimited Data</label>
+                  <c-switch
+                    color="primary"
+                    :checked="unlimitedSwitches.internationalData"
+                    @change="unlimitedSwitches.internationalData = !unlimitedSwitches.internationalData"
+                  />
+                </div>
               </div>
               <div class="col-sm-4 col-xs-12">
                 <label><i class="fa fa-commenting"></i> SMS</label>
@@ -176,8 +245,20 @@
                     <b-input-group-text>Amount</b-input-group-text>
                   </b-input-group-prepend>
                   <b-form-input type="number" class="form-control" :value="internationalPlan.sms.value"
-                                @input="$store.commit('service/updateInternationalplan',{e:$event,type:'sms'})" />
+                    @input="$store.commit('service/updateInternationalplan',{e:$event,type:'sms'})"
+                    :state="formValidators.flags.internationalSMSErr"
+                    :disabled="!unlimitedSwitches.internationalSMS"
+                    />
+                  <p v-if="formValidators.flags.internationalSMSErr === false" class="mb-0 mt-2 text-danger">Must be between 0 and 999.999</p>
                 </b-input-group>
+                <div class="d-flex">
+                  <label class="block">Unlimited SMS</label>
+                  <c-switch
+                    color="primary"
+                    :checked="unlimitedSwitches.internationalSMS"
+                    @change="unlimitedSwitches.internationalSMS = !unlimitedSwitches.internationalSMS"
+                  />
+                </div>
               </div>
             </div>
           </b-card-body>
@@ -207,20 +288,23 @@
                   <b-input-group-prepend>
                     <b-input-group-text>Amount</b-input-group-text>
                   </b-input-group-prepend>
-                  <b-form-input type="number" class="form-control" :value="addon.cost"
-                                @keyup="$store.commit('service/updateAddon',{i:index,e:$event,type:'price'})" />
+                  <b-form-input type="number" class="form-control"
+                    :value="addon.cost"
+                    :state="formValidators.addonFlags['addon' + index.toString() + 'Cost']"
+                    @keyup="$store.commit('service/updateAddon',{i:index,e:$event,type:'price'})" />
                   <b-input-group-prepend>
                     <b-input-group-text>{{ serviceDetails.currency }}</b-input-group-text>
                   </b-input-group-prepend>
+                  <p v-if="(formValidators.addonFlags['addon' + index.toString() + 'Cost']) === false" class="mb-0 mt-2 text-danger">Must be between 0 and 999.999</p>
                 </b-input-group>
               </div>
               <div class="col-sm-4 col-xs-12">
                 <b-button class="btn-control" variant="danger" aria-pressed="true"
-                          @click="$store.commit('service/deleteAddOns',index)">X</b-button>
+                          @click="$store.commit('service/deleteAddOns',index); removeFormAddonValidator();">X</b-button>
               </div>
             </div>
             <div class="input-group-wrapper">
-              <b-button variant="primary" @click="$store.commit('service/hideAndPush')">
+              <b-button variant="primary" @click="$store.commit('service/hideAndPush'); addFormAddonValidator();">
                 <i class="fa fa-lightbulb-o"></i>&nbsp; Add New Field
               </b-button>
             </div>
@@ -229,6 +313,8 @@
       </div>
 
       <b-button variant="primary" class="mb-4" @click="save()" id="button">Save Changes</b-button>
+      <p v-if="formValidators.generalErr" class="mb-0 mt-2 text-danger">Some errors were found. Check the fields above for more details</p>
+
     </div>
   </div>
 </template>
