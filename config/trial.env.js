9beb671b744ac19d7b836232d4501f31d52cd691
opensource1030 <opensource1030@gmail.com>
2019-09-23 23:57:00 -0400

    bump release

--- config/trial.env.js
+++ config/trial.env.js
@@ -10,6 +10,6 @@ module.exports = {
   EV_ACCOUNT: 50005,
   EASYVISTA_CODE: 13,
   EZW_SSO_URL: 'https://oauth2.eztest.nu/sso?partner=WIRELESS',
-  VERSION:'v2019.9.2',
+  VERSION:'v2019.9.3',
   EV_URL: 'https://wa.easyvista.com/api/v1'
 };
