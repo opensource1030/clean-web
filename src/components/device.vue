c0bc159953bf41d8908a85bccc17d05b4d32ac0a
opensource1030 <opensource1030@gmail.com>
2019-10-10 15:40:48 +0000

    #707 - refactor device selection (#722)
    * #707 - update device selection
    
    * update select, button style in device selection

--- src/components/device.vue
+++ src/components/device.vue
@@ -8,48 +8,80 @@
 
       <div class="device-info">
         <div class="d-flex justify-content-between">
+          <h3>{{ deviceName }}</h3>
           <div>
-            <div class="device-name">{{ deviceName }}</div>
-
-            <div v-for="(values, key) in allVariations" class="d-flex align-items-center mt-3">
-              <div
-                v-for="value of values"
-                class="device-mod mr-2"
-                :class="getModClass(key, value)"
-                @click="toggleDeviceMod(key, value)"
-              >
-                <span v-if="key !== 'style'">{{ value }}</span>
-              </div>
-            </div>
+            <span>Start from</span>
+            <h3>{{ startVariation.price1 }}</h3>
           </div>
 
-          <div class="device-price">
-            <template v-if="selectedDevice">
-              <div class="device-price-item">
-                <span>Price</span>
-                <span>
-                  <b>$ {{ selectedDevice.price1 }}</b>
-                </span>
-              </div>
+          <!-- <div v-for="(values, key) in allVariations" class="d-flex align-items-center mt-3">
+            <div
+              v-for="value of values"
+              class="device-mod mr-2"
+              :class="getModClass(key, value)"
+              @click="toggleDeviceMod(key, value)"
+            >
+              <span v-if="key !== 'style'">{{ value }}</span>
+            </div>
+          </div> -->
+        </div>
 
-              <div class="device-price-item">
-                <span>Retail</span>
-                <span>$ {{ selectedDevice.priceRetail }}</span>
-              </div>
-            </template>
-            <span v-else class="device-price-not-available">Select size and color</span>
+        <div class="device-price">
+          <div v-if="selectedDevice" class="device-price-item">
+            <span>Retail</span>
+            <span>$ {{ selectedDevice.priceRetail }}</span>
+          </div>
+          <div v-else class="device-price-item">
+            <span>Retail</span>
+            <span class="device-price-not-available">$ {{ startVariation.priceRetail }}</span>
           </div>
         </div>
 
-        <div class="device-order mt-4">
+        <div class="row mt-3">
+          <div class="col pr-0">
+            <b-form-select
+              class="mb-1"
+              name="color"
+              v-model="selected.color"
+              :options="colors"
+              value-field="id"
+              text-field="value"
+              @input="toggleDeviceMod"
+            >
+              <template v-slot:first>
+                <option :value="null" disabled>Select color</option>
+              </template>
+            </b-form-select>
+          </div>
+          <div class="col pl-2">
+            <b-form-select
+              class="mb-1"
+              name="capacity"
+              v-model="selected.capacity"
+              :options="capacities"
+              value-field="id"
+              text-field="value"
+              @input="toggleDeviceMod"
+            >
+              <template v-slot:first>
+                <option :value="null" disabled>Select capacity</option>
+              </template>
+            </b-form-select>
+          </div>
+        </div>
+
+        <div class="device-order mt-3">
           <b-btn
             variant="default"
             size="sm"
             :disabled="!selectedDevice"
+            class="w-100"
             @click="onRequestDevice"
-          >Request Device</b-btn>
+          >Choose device</b-btn>
+        </div>
 
-          <span v-if="!!deviceProperty" class="device-toggle-property pl-5" @click="toggleProperty">
+        <div class="text-right mt-2">
+          <span class="device-toggle-property" @click="toggleProperty">
             {{ showDeviceProperty ? 'Hide info' : 'Show info' }}
             <i
               v-if="showDeviceProperty"
@@ -74,7 +106,10 @@ export default {
 
   data() {
     return {
-      selected: {},
+      selected: {
+        color: null,
+        capacity: null
+      },
       showDeviceProperty: false
     };
   },
@@ -97,36 +132,45 @@ export default {
       return imageUrl ? `https://${imageUrl}` : imageUrl;
     },
 
-    allVariations() {
-      let allVariations = {};
-      this.variations.forEach(variation => {
-        const { modifications } = variation;
+    // allVariations() {
+    //   let allVariations = {};
+    //   this.variations.forEach(variation => {
+    //     const { modifications } = variation;
+    //     modifications.forEach(modification => {
+    //       const { modType, value } = modification;
+    //       if (Object.keys(allVariations).indexOf(modType) === -1) {
+    //         allVariations[modType] = [];
+    //       }
+    //       if (allVariations[modType].indexOf(value) === -1) {
+    //         allVariations[modType].push(value);
+    //       }
+    //     });
+    //   });
+    //   return allVariations;
+    // },
 
-        modifications.forEach(modification => {
-          const { modType, value } = modification;
+    colors() {
+      return _.chain(this.variations).flatMap('modifications').uniq('id').filter({modType: 'style'}).value()
+    },
 
-          if (Object.keys(allVariations).indexOf(modType) === -1) {
-            allVariations[modType] = [];
-          }
+    capacities() {
+      return _.chain(this.variations).flatMap('modifications').uniq('id').filter({modType: 'capacity'}).value()
+    },
 
-          if (allVariations[modType].indexOf(value) === -1) {
-            allVariations[modType].push(value);
-          }
-        });
-      });
-
-      return allVariations;
+    startVariation() {
+      return _.get(this.variations, "0");
     },
 
     selectedDevice() {
-      if (Object.values(this.selected).indexOf(null) !== -1) {
+      if (!this.selected.color || !this.selected.capacity) {
         return null;
       }
 
-      const device = this.deviceWithMods.find(device =>
-        _.isEqual(device.modification, this.selected)
-      );
+      const device = this.variations.find(dv => {
+        return _.find(dv.modifications, { id: this.selected.color }) && _.find(dv.modifications, { id: this.selected.capacity })
+      });
 
+      // console.log('selectedDevice', device)
       return device;
     },
 
@@ -158,6 +202,9 @@ export default {
   },
 
   created() {
+    // console.log('device')
+    // console.log(this.variations)
+    // console.log(this.initialValue)
     this.populateSelected(this.initialValue);
   },
 
@@ -173,21 +220,22 @@ export default {
     },
 
     populateSelected(value) {
+      // if (value) {
+      //   this.selected = { ...value.modification };
+      // } else {
+      //   let availableModTypes = {};
+      //   this.variations[0].modifications.forEach(modification => {
+      //     availableModTypes[modification.modType] = null;
+      //   });
+      //   this.selected = { ...availableModTypes };
+      // }
       if (value) {
-        this.selected = { ...value.modification };
-      } else {
-        let availableModTypes = {};
-        this.variations[0].modifications.forEach(modification => {
-          availableModTypes[modification.modType] = null;
-        });
-
-        this.selected = { ...availableModTypes };
+        this.selected.color = _.get(_.find(value.modifications, { modType: 'style' }), 'id')
+        this.selected.capacity = _.get(_.find(value.modifications, { modType: 'capacity' }), 'id')
       }
     },
 
-    toggleDeviceMod(key, value) {
-      this.selected[key] = value === this.selected[key] ? null : value;
-
+    toggleDeviceMod() {
       if (!!this.selectedDevice) {
         this.$emit("selectDevice", {
           ...this.selectedDevice,
@@ -197,15 +245,15 @@ export default {
       }
     },
 
-    getModClass(key, value) {
-      return Object.assign(
-        {
-          [`device-${key}`]: true,
-          [`device-${key}--active`]: this.selected[key] === value
-        },
-        key === "style" && { [`mod-${_.kebabCase(value)}`]: true }
-      );
-    },
+    // getModClass(key, value) {
+    //   return Object.assign(
+    //     {
+    //       [`device-${key}`]: true,
+    //       [`device-${key}--active`]: this.selected[key] === value
+    //     },
+    //     key === "style" && { [`mod-${_.kebabCase(value)}`]: true }
+    //   );
+    // },
 
     toggleProperty() {
       this.showDeviceProperty = !this.showDeviceProperty;
@@ -217,6 +265,29 @@ export default {
 <style lang="scss" scoped>
 .device {
   margin: 3rem 0;
+
+  h3 {
+    display: inline-block;
+    color: #1f2027;
+    font-weight: 600;
+    font-size: 14px;
+  }
+
+  select {
+    padding: 6px 15px 6px 6px;
+    font-size: 11px;
+    background: none;
+  }
+
+  // .btn.disabled, .btn:disabled {
+  //   opacity: 0.65;
+  //   background-color: #a0a0a0;
+  //   border-color: #a0a0a0;
+  //   &:hover {
+  //     background-color: #a0a0a0;
+  //     border-color: #a0a0a0;
+  //   }
+  // }
 
   &-thumb {
     width: 170px;
@@ -241,93 +312,84 @@ export default {
     width: 100%;
   }
 
-  &-name {
-    color: #1f2027;
-    font-weight: 600;
-    font-size: 14px;
-  }
+  // &-mod {
+  //   user-select: none;
+  //   cursor: pointer;
+  // }
 
-  &-mod {
-    user-select: none;
-    cursor: pointer;
-  }
+  // &-style {
+  //   width: 12px;
+  //   height: 12px;
+  //   background-color: #ffbdbd;
+  //   box-sizing: content-box;
+  //   border-radius: 100%;
+  //   border: 2px solid transparent;
+  //   position: relative;
+  //   &:not(:last-of-type) {
+  //     margin-right: 10px;
+  //   }
+  //   &--active,
+  //   &:hover {
+  //     &:before {
+  //       content: "";
+  //       position: absolute;
+  //       left: -5px;
+  //       top: -5px;
+  //       right: -5px;
+  //       bottom: -5px;
+  //       background-color: transparent;
+  //       border: 1px solid #20a8d8;
+  //       border-radius: 100%;
+  //     }
+  //   }
+  // }
 
-  &-style {
-    width: 12px;
-    height: 12px;
-    background-color: #ffbdbd;
-    box-sizing: content-box;
-    border-radius: 100%;
-    border: 2px solid transparent;
-    position: relative;
+  // &-capacity {
+  //   color: #20a8d8;
+  //   padding: 5px;
+  //   font-weight: 600;
+  //   border-radius: 5px;
+  //   &:not(:last-of-type) {
+  //     margin-right: 6px;
+  //   }
+  //   &--active,
+  //   &:hover {
+  //     background-color: rgba(32, 168, 216, 0.21);
+  //   }
+  // }
 
-    &:not(:last-of-type) {
-      margin-right: 10px;
-    }
-
-    &--active,
-    &:hover {
-      &:before {
-        content: "";
-        position: absolute;
-        left: -5px;
-        top: -5px;
-        right: -5px;
-        bottom: -5px;
-        background-color: transparent;
-        border: 1px solid #20a8d8;
-        border-radius: 100%;
-      }
-    }
-  }
-
-  &-capacity {
-    color: #20a8d8;
-    padding: 5px;
-    font-weight: 600;
-    border-radius: 5px;
-
-    &:not(:last-of-type) {
-      margin-right: 6px;
-    }
-
-    &--active,
-    &:hover {
-      background-color: rgba(32, 168, 216, 0.21);
-    }
-  }
+  // &-price {
+  //   color: #657089;
+  //   font-size: 10px;
+  //   display: flex;
+  //   flex-direction: column;
+  //   min-width: 120px;
+  //   &-item {
+  //     display: flex;
+  //     align-items: baseline;
+  //     white-space: nowrap;
+  //     span {
+  //       width: 50%;
+  //       text-align: end;
+  //     }
+  //     span:last-child {
+  //       font-weight: 600;
+  //       font-size: 12px;
+  //       padding-left: 5px;
+  //       text-align: start;
+  //     }
+  //     b {
+  //       color: #1f2027;
+  //     }
+  //   }
+  //   &-not-available {
+  //     text-align: end;
+  //   }
+  // }
 
   &-price {
-    color: #657089;
-    font-size: 10px;
-    display: flex;
-    flex-direction: column;
-    min-width: 120px;
-
-    &-item {
-      display: flex;
-      align-items: baseline;
-      white-space: nowrap;
-
-      span {
-        width: 50%;
-        text-align: end;
-      }
-
-      span:last-child {
-        font-weight: 600;
-        font-size: 12px;
-        padding-left: 5px;
-        text-align: start;
-      }
-
-      b {
-        color: #1f2027;
-      }
-    }
-
     &-not-available {
-      text-align: end;
+      text-decoration: line-through;
     }
   }
 
